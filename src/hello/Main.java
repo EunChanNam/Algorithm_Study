4ac72339f5ffd31366c6702378142d49fae3a7f8
eunchannam <namhm23@gmail.com>
2022-11-15 10:01:29 +0900

    백준 1600번

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -1,61 +1,86 @@
 package hello;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static class Point {
-        Integer x;
-        Integer y;
-        public Point(Integer x, Integer y) {
-            this.x = x;
+    static int k;
+    static int n1, n2;
+    static int[][] dir = {
+            {-1, 0}, {0, -1}, {0, 1}, {1, 0}
+    };
+    static int[][] horse = {
+            {-1, -2}, {-2, -1}, {-2, 1}, {-1, 2},
+            {1, -2}, {2, -1}, {2, 1}, {1, 2}
+    };
+    static int[][] map;
+    static int[][][] visit;
+
+    static class Node{
+        int y;
+        int x;
+        int level;
+        int horseCnt;
+
+        public Node(int y, int x, int level, int horseCnt) {
             this.y = y;
+            this.x = x;
+            this.level = level;
+            this.horseCnt = horseCnt;
         }
     }
-    public static List<Point> solution(int n, int[][] a) {
-        List<Point> answer = new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
-            Point point = new Point(a[i][0], a[i][1]);
-            answer.add(point);
-        }
+    static int bfs(Node node) {
+        Queue<Node> que = new LinkedList<>();
+        visit[0][0][0] = 1;
+        que.offer(node);
 
-        Comparator<Point> comparator = (p1, p2) -> {
-            if (p1.x.equals(p2.x)) {
-                return p1.y.compareTo(p2.y);
-            } else return p1.x.compareTo(p2.x);
-        };
-
-        answer.sort(comparator);
-
-        return answer;
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int n1 = sc.nextInt();
-//        int n2 = sc.nextInt();
-        int[][] a = new int[n1][2];
-        for (int y = 0; y < n1; y++) {
-            for (int x=0; x< 2; x++){
-                a[y][x] = sc.nextInt();
+        while (!que.isEmpty()) {
+            Node p = que.poll();
+            if (p.y == n2 - 1 && p.x == n1 - 1) {
+                return p.level;
+            }
+            for (int i = 0; i < 4; i++) {
+                int ny = p.y + dir[i][0];
+                int nx = p.x + dir[i][1];
+                if (ny < 0 || ny >= n2 || nx < 0 || nx >= n1) continue;
+                if (map[ny][nx] == 1) continue;
+                if (visit[p.horseCnt][ny][nx] == 1) continue;
+                visit[p.horseCnt][ny][nx] = 1;
+                que.offer(new Node(ny, nx, p.level + 1, p.horseCnt));
+            }
+            if (p.horseCnt < k) {
+                for (int i = 0; i < 8; i++) {
+                    int ny = p.y + horse[i][0];
+                    int nx = p.x + horse[i][1];
+                    if (ny < 0 || ny >= n2 || nx < 0 || nx >= n1) continue;
+                    if (map[ny][nx] == 1) continue;
+                    if (visit[p.horseCnt + 1][ny][nx] == 1) continue;
+                    visit[p.horseCnt + 1][ny][nx] = 1;
+                    que.offer(new Node(ny, nx, p.level + 1, p.horseCnt + 1));
+                }
             }
         }
-//        String str1 = sc.next();
-//        String str2 = sc.next();
-//        int[] a = new int[n1];
-//        for (int i = 0; i < n1; i++) {
-//            a[i] = sc.nextInt();
-//        }
-//        int[] b = new int[n2];
-//        for (int i = 0; i < n2; i++) {
-//            b[i] = sc.nextInt();
-//        }
-        List<Point> ret = solution(n1, a);
-//        ret.forEach((s) -> System.out.print(s + " "));
-//        for (int r : ret) {
-//            System.out.print(r + " ");
-//        }
-//        System.out.println(ret);
-        ret.forEach(p -> System.out.println(p.x + " " + p.y));
+        return -1;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        k = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n1 = Integer.parseInt(st.nextToken());
+        n2 = Integer.parseInt(st.nextToken());
+        map = new int[n2][n1];
+        visit = new int[k + 1][n2][n1];
+
+        for (int y = 0; y < n2; y++) {
+            st = new StringTokenizer(br.readLine());
+            for (int x = 0; x < n1; x++) {
+                map[y][x] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        System.out.println(bfs(new Node(0, 0, 0, 0)));
     }
 }
