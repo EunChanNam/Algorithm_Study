73aca29e86dfff93e27cc63d3c89c1ba98f2b589
eunchannam <namhm23@gmail.com>
2022-12-03 01:08:25 +0900

    프림 알고리즘(PriorityQueue)

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -5,40 +5,49 @@ import java.util.*;
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static int n;
-    static List<Node> list = new ArrayList<>();
-    static PriorityQueue<Integer> que = new PriorityQueue<>(Collections.reverseOrder());
-
+    static int m;
+    static int[] visit;
+    static List<List<Node>> list = new ArrayList<>();
+    static PriorityQueue<Node> que = new PriorityQueue<>((a, b) -> Integer.compare(a.cost, b.cost));
     static class Node{
-        int k; int d;
-        public Node(int k, int d) {
-            this.k = k;
-            this.d = d;
+        int a; int cost;
+        public Node(int a, int cost) {
+            this.a = a;
+            this.cost = cost;
         }
     }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        n = Integer.parseInt(br.readLine());
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            int k = Integer.parseInt(st.nextToken());
-            int d = Integer.parseInt(st.nextToken());
-            list.add(new Node(k, d));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        visit = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            list.add(new ArrayList<>());
         }
-        list.sort((a, b) -> {
-            if (a.d == b.d) return Integer.compare(b.k, a.k);
-            return Integer.compare(b.d, a.d);
-        });
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            list.get(a).add(new Node(b, c));
+            list.get(b).add(new Node(a, c));
+        }
 
-        int i = list.get(0).d;
-        int sum =0;
-        int x = 0;
-        for (int y = i; y >= 1; y--) {
-            for ( ; x < n; x++) {
-                if (list.get(x).d < y) break;
-                que.offer(list.get(x).k);
+        int cnt = 0;
+        int sum = 0;
+        que.add(new Node(1, 0));
+        while(cnt < n){
+            Node p = que.poll();
+            if (visit[p.a] != 1){
+                for(Node next : list.get(p.a)){
+                    que.offer(next);
+                }
+                visit[p.a] = 1;
+                cnt++;
+                sum += p.cost;
             }
-            if (!que.isEmpty()) sum += que.poll();
         }
 
         bw.write(String.valueOf(sum));
