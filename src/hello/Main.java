61ab3cea2e448fd722ebc9eeb81a03f17a832389
eunchannam <namhm23@gmail.com>
2022-12-09 12:38:40 +0900

    프로그래머스 숫자 카드 나누기

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -3,78 +3,46 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[][] dir = {{-1,0}, {0,-1}, {0,1}, {1,0}};
-    static int[][] visit;
-    static Queue<Point> que = new LinkedList<>();
-    static class Point{
-        int y; int x; int level;
-        public Point(int y, int x, int level){
-            this.y = y;
-            this.x = x;
-            this.level = level;
-        }
-    }
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
 
-    static boolean isUnable(Point start, String[] map) {
-        que.offer(start);
-        visit[start.y][start.x] = 1;
+        Arrays.sort(a);
+        Arrays.sort(b);
+        int max = 0;
 
-        while(!que.isEmpty()){
-            Point p = que.poll();
-            if (p.level == 2) continue;
-            for (int i=0; i < 4; i++){
-                int ny = p.y + dir[i][0];
-                int nx = p.x + dir[i][1];
-                if (ny < 0 || ny >= 5 || nx < 0 || nx >= 5) continue;
-                if (visit[ny][nx] == 1) continue;
-                if (map[ny].charAt(nx) == 'X') continue;
-                if (map[ny].charAt(nx) == 'P') {
-                    que.clear();
-                    return true;
+        for (int i=a[0]; i >= 1; i--){
+            boolean flag2 = true;
+            for (int x=0; x < a.length; x++){
+                if (a[x] % i != 0 || b[x] % i == 0){
+                    flag2 = false;
+                    break;
                 }
-                visit[ny][nx] = 1;
-                que.offer(new Point(ny, nx, p.level + 1));
+            }
+            if (flag2) {
+                max = i;
+                break;
             }
         }
-        return false;
-    }
-
-    public static int[] solution(String[][] place) {
-        int[] answer = new int[place.length];
-        int n = place.length;
-
-        for (int z=0; z < n; z++){
-            boolean flag = false;
-            for (int y=0; y < 5; y++){
-                for (int x=0; x < 5; x++){
-                    if (place[z][y].charAt(x) == 'P'){
-                        visit = new int[5][5];
-                        if (isUnable(new Point(y, x, 0), place[z])){
-                            flag = true;
-                            break;
-                        }
-                    }
+        for (int i=b[0]; i >= 1; i--){
+            boolean flag = true;
+            for (int x=0; x < b.length; x++){
+                if (b[x] % i != 0 || a[x] % i == 0){
+                    flag = false;
+                    break;
                 }
-                if (flag) break;
             }
-            if (flag) answer[z] = 0;
-            else answer[z] = 1;
+            if (flag) {
+                max = Math.max(max, i);
+                break;
+            }
         }
+
+        answer = max;
 
         return answer;
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String[][] place = {{"POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"},
-                {"POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"},
-                {"PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"},
-                {"OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"},
-                {"PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"}};
-        int[] solution = solution(place);
 
-        for (int answer : solution) {
-            System.out.print(answer + " ");
-        }
     }
 }
