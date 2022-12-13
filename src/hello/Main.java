04ebf01376a5779a644015b9ca81bc4abbec493d
eunchannam <namhm23@gmail.com>
2022-12-13 15:24:16 +0900

    백준 14500 테트로미노

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -5,21 +5,44 @@ import java.util.*;
 public class Main {
     static int n;
     static int m;
-    static char[][] map;
-    static int[][] dir = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
-    static Map<Character, Integer> check = new HashMap<>();
+    static int[][] map;
+    static int[][] dir = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
     static int max = Integer.MIN_VALUE;
+    static int[][] visit;
 
-    static void dfs(int dy, int dx) {
-        max = Math.max(max, check.size());
+    static void dfs(int level, int sum, int y, int x) {
+        if (level == 4) {
+            max = Math.max(max, sum);
+            return;
+        }
         for (int i = 0; i < 4; i++) {
-            int ny = dy + dir[i][0];
-            int nx = dx + dir[i][1];
-            if (ny <0 || nx < 0 || ny >= n || nx >= m) continue;
-            if (check.containsKey(map[ny][nx])) continue;
-            check.put(map[ny][nx], 1);
-            dfs(ny, nx);
-            check.remove(map[ny][nx]);
+            int ny = y + dir[i][0];
+            int nx = x + dir[i][1];
+            if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+            if (visit[ny][nx] == 1) continue;
+            visit[ny][nx] = 1;
+            dfs(level + 1, sum + map[ny][nx], ny, nx);
+            visit[ny][nx] = 0;
+        }
+    }
+
+    static void processT(int y, int x) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            int ny = y + dir[i][0];
+            int nx = x + dir[i][1];
+            if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+            list.add(map[ny][nx]);
+        }
+        if (list.size() < 3) return;
+        else if (list.size() == 3) {
+            int sum = list.stream().reduce(Integer::sum).get();
+            max = Math.max(max, sum + map[y][x]);
+        } else {
+            int sum = list.stream().reduce(Integer::sum).get();
+            for (int i = 0; i < 4; i++) {
+                max = Math.max(max, sum - list.get(i) + map[y][x]);
+            }
         }
     }
 
@@ -29,18 +52,23 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        map = new char[n][m];
-
+        map = new int[n][m];
         for (int y = 0; y < n; y++) {
-            String temp = br.readLine();
+            st = new StringTokenizer(br.readLine());
             for (int x = 0; x < m; x++) {
-                char ch = temp.charAt(x);
-                map[y][x] = ch;
+                map[y][x] = Integer.parseInt(st.nextToken());
             }
         }
+        visit = new int[n][m];
 
-        check.put(map[0][0], 1);
-        dfs(0, 0);
+        for (int y = 0; y < n; y++) {
+            for (int x=0; x < m; x++){
+                visit[y][x] = 1;
+                dfs(1, map[y][x], y, x);
+                visit[y][x] = 0;
+                processT(y, x);
+            }
+        }
 
         bw.write(String.valueOf(max));
 
