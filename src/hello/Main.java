f215a91dc34d22aff70b31134860f1b478fec51d
eunchannam <namhm23@gmail.com>
2022-12-14 15:22:39 +0900

    백준 1931 회의실 배정

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -4,73 +4,45 @@ import java.util.*;
 
 public class Main {
     static int n;
-    static int m;
-    static int[][] map;
-    static int[][] dir = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
-    static int max = Integer.MIN_VALUE;
-    static int[][] visit;
-
-    static void dfs(int level, int sum, int y, int x) {
-        if (level == 4) {
-            max = Math.max(max, sum);
-            return;
-        }
-        for (int i = 0; i < 4; i++) {
-            int ny = y + dir[i][0];
-            int nx = x + dir[i][1];
-            if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-            if (visit[ny][nx] == 1) continue;
-            visit[ny][nx] = 1;
-            dfs(level + 1, sum + map[ny][nx], ny, nx);
-            visit[ny][nx] = 0;
-        }
-    }
-
-    static void processT(int y, int x) {
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < 4; i++) {
-            int ny = y + dir[i][0];
-            int nx = x + dir[i][1];
-            if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-            list.add(map[ny][nx]);
-        }
-        if (list.size() < 3) return;
-        else if (list.size() == 3) {
-            int sum = list.stream().reduce(Integer::sum).get();
-            max = Math.max(max, sum + map[y][x]);
-        } else {
-            int sum = list.stream().reduce(Integer::sum).get();
-            for (int i = 0; i < 4; i++) {
-                max = Math.max(max, sum - list.get(i) + map[y][x]);
-            }
+    static Node[] arr;
+    static class Node{
+        int start; int end;
+        public Node(int start, int end){
+            this.start = start;
+            this.end = end;
         }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        map = new int[n][m];
-        for (int y = 0; y < n; y++) {
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+        arr = new Node[n];
+        for (int i=0; i < n; i++){
             st = new StringTokenizer(br.readLine());
-            for (int x = 0; x < m; x++) {
-                map[y][x] = Integer.parseInt(st.nextToken());
-            }
-        }
-        visit = new int[n][m];
-
-        for (int y = 0; y < n; y++) {
-            for (int x=0; x < m; x++){
-                visit[y][x] = 1;
-                dfs(1, map[y][x], y, x);
-                visit[y][x] = 0;
-                processT(y, x);
-            }
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            arr[i] = new Node(a, b);
         }
 
-        bw.write(String.valueOf(max));
+        Arrays.sort(arr, (q, w) -> {
+            if (q.end == w.end) return Integer.compare(q.start, w.start);
+            else return Integer.compare(q.end, w.end);
+        });
+
+        int now = arr[0].end;
+        int cnt = 1;
+        for (int i=1; i < n; i++){
+            Node p = arr[i];
+            if (p.start >= now){
+                cnt++;
+                now = p.end;
+            }
+        }
+
+        bw.write(String.valueOf(cnt));
 
         br.close();
         bw.close();
