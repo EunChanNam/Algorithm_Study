7e444cdfb68fdd7d398b088601374f7abbc680f0
eunchannam <namhm23@gmail.com>
2022-12-16 16:40:53 +0900

    백준 1138 한 줄로 서기

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -4,7 +4,8 @@ import java.util.*;
 
 public class Main {
     static int n;
-    static PriorityQueue<Integer> que = new PriorityQueue<>();
+    static int[] arr;
+
 
 
     public static void main(String[] args) throws IOException {
@@ -12,25 +13,30 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
         n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            int a = Integer.parseInt(br.readLine());
-            arr[i] = a;
-        }
-        for (int a : arr) {
-            que.offer(a);
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int answer = 0;
-        while (que.size() != 1) {
-            int a = que.poll();
-            int b = que.poll();
-            int sum = a + b;
-            answer += sum;
-            que.offer(sum);
+        int[] answer = new int[n];
+        for (int i = 0; i < n; i++) {
+            int cnt = 0;
+            for (int x = 0; x < n; x++) {
+                if (cnt == arr[i]) {
+                    int t = x;
+                    while (answer[t] != 0) t++;
+                    answer[t] = i + 1;
+                    break;
+                }
+                if (answer[x] == 0) cnt++;
+            }
         }
 
-        bw.write(String.valueOf(answer));
+        for (int i = 0; i < n; i++) {
+            if (i == n - 1) bw.write(String.valueOf(answer[i]));
+            else bw.write(answer[i] + " ");
+        }
 
         br.close();
         bw.close();
