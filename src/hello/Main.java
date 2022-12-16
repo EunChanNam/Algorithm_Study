b6a358ec92d85e05b57c3368b72240136f8ddaed
eunchannam <namhm23@gmail.com>
2022-12-16 12:21:30 +0900

    백준 16236 아기상어

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -4,12 +4,58 @@ import java.util.*;
 
 public class Main {
     static int n;
-    static PriorityQueue<Node> que = new PriorityQueue<>((a, b) -> Integer.compare(b.val, a.val));
+    static int[][] map;
+    static int[][] visit;
+    static int dy;
+    static int dx;
+    static int size = 2;
+    static int sizeCnt = 0;
+    static boolean isMoved;
+    static int[][] dir = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+    //같은 거리라면 위에, 그리고 왼쪽에 있는 먹이를 먼저 먹으므로 우선순위 큐를 사용해서 큐를 거리(level), y, x 순으로 정렬한다.
+    static PriorityQueue<Node> que = new PriorityQueue<>((q, w) -> {
+        if (q.level == w.level){
+            if (q.y == w.y) return Integer.compare(q.x, w.x);
+            else return Integer.compare(q.y, w.y);
+        } else return Integer.compare(q.level, w.level);
+    });
+    static int answer = 0;
     static class Node{
-        int dis; int val;
-        public Node(int dis, int val){
-            this.dis = dis;
-            this.val = val;
+        int y; int x; int level;
+        public Node(int y, int x, int level){
+            this.y = y;
+            this.x = x;
+            this.level = level;
+        }
+    }
+
+    static void bfs() {
+        que.offer(new Node(dy, dx, 0));
+        visit[dy][dx] = 1;
+        while (!que.isEmpty()) {
+            Node p = que.poll();
+            if (p.level != 0 && map[p.y][p.x] < size && map[p.y][p.x] != 0) {
+                map[dy][dx] = 0;
+                dy = p.y;
+                dx = p.x;
+                answer += p.level;
+                sizeCnt++;
+                if (sizeCnt == size) {
+                    size++; sizeCnt = 0;
+                }
+                isMoved = true;
+                que.clear();
+                break;
+            }
+            for (int i = 0; i < 4; i++) {
+                int ny = p.y + dir[i][0];
+                int nx = p.x + dir[i][1];
+                if (ny < 0 || nx < 0 || ny >= n ||nx >= n) continue;
+                if (visit[ny][nx] == 1) continue;
+                if (map[ny][nx] > size) continue;
+                visit[ny][nx] = 1;
+                que.offer(new Node(ny, nx, p.level + 1));
+            }
         }
     }
 
@@ -17,36 +63,28 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
-
         n = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < n; i++) {
+        map = new int[n][n];
+        for (int y = 0; y < n; y++) {
             st = new StringTokenizer(br.readLine());
-            int q = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            que.offer(new Node(q, w));
-        }
-        st = new StringTokenizer(br.readLine());
-        int goal = Integer.parseInt(st.nextToken());
-        int start = Integer.parseInt(st.nextToken());
-
-        int cnt = 0;
-        int sum = start;
-        List<Node> list = new ArrayList<>();
-        while (sum < goal && !que.isEmpty()) {
-            Node p = que.poll();
-            if (sum >= p.dis) {
-                sum += p.val;
-                cnt++;
-                for (Node t : list) {
-                    que.offer(t);
+            for (int x = 0; x < n; x++) {
+                map[y][x] = Integer.parseInt(st.nextToken());
+                if (map[y][x] == 9) {
+                    dy = y;
+                    dx = x;
                 }
-                list.clear();
-            } else list.add(p);
+            }
         }
 
-        if (que.isEmpty()) bw.write(String.valueOf(-1));
-        else bw.write(String.valueOf(cnt));
+        boolean keep = true;
+        while (keep) {
+            visit = new int[n][n];
+            isMoved = false;
+            bfs();
+            if (!isMoved) keep = false;
+        }
+
+        bw.write(String.valueOf(answer));
 
         br.close();
         bw.close();
