14e399f6b870a918f286834aa183a56bc3d59c09
eunchannam <namhm23@gmail.com>
2022-12-16 13:41:24 +0900

    백준 1715 카드 정렬하기

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -4,45 +4,30 @@ import java.util.*;
 
 public class Main {
     static int n;
-    static Node[] arr;
-    static PriorityQueue<Integer> que = new PriorityQueue<>(Comparator.reverseOrder());
-    static class Node{
-        int val; int d;
-        public Node(int val, int d){
-            this.val = val;
-            this.d = d;
-        }
-    }
+    static PriorityQueue<Integer> que = new PriorityQueue<>();
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
         n = Integer.parseInt(br.readLine());
-        if (n == 0) {
-            bw.write("0");
-            br.close();
-            bw.close();
-            return;
-        }
-        arr = new Node[n];
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            arr[i] = new Node(a, b);
+            int a = Integer.parseInt(br.readLine());
+            arr[i] = a;
         }
-        Arrays.sort(arr, (q, w) -> Integer.compare(w.d, q.d));
+        for (int a : arr) {
+            que.offer(a);
+        }
 
         int answer = 0;
-        int start = arr[0].d;
-        int x = 0;
-        for (int y = start; y >= 1; y--) {
-            for (; x < n; x++) {
-                if (arr[x].d < y) break;
-                que.offer(arr[x].val);
-            }
-            if (!que.isEmpty()) answer += que.poll();
+        while (que.size() != 1) {
+            int a = que.poll();
+            int b = que.poll();
+            int sum = a + b;
+            answer += sum;
+            que.offer(sum);
         }
 
         bw.write(String.valueOf(answer));
