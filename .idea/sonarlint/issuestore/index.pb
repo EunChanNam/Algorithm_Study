2b4259084b48c0e6d12d7276cd2a84d47d2a87a5
남은찬 <eunchan@nam-eunchan-ui-MacBookAir.local>
2022-12-19 15:32:31 +0900

    Study : 가장 높은 탑 쌓기(LIS)

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+C
+src/hello/Main.java,2/4/24c032be00a66065f0fafa9fcd21e29843f1da3b
--- src/hello/Main.java
+++ src/hello/Main.java
@@ -4,28 +4,42 @@ import java.util.*;
 
 public class Main {
     static int n;
+    static class Node{
+        int s; int h; int w;
+        public Node(int s, int h, int w){
+            this.s = s;
+            this.h = h;
+            this.w = w;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
         n = Integer.parseInt(br.readLine());
-        int[] a = new int[n];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            a[i] = Integer.parseInt(st.nextToken());
-        }
+        Node[] a = new Node[n];
 
-        int answer = 1;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int h = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            a[i] = new Node(s, h, w);
+        }
+        Arrays.sort(a, (q, w) -> Integer.compare(w.s, q.s));
+
         int[] dp = new int[n];
-        dp[0] = 1;
+        dp[0] = a[0].h;
+
+        int answer = dp[0];
         for (int i = 1; i < n; i++) {
-            for (int x = i - 1; x >= 0; x--) {
-                if (a[x] < a[i]) {
-                    dp[i] = Math.max(dp[i], dp[x]);
+            for (int j = i - 1; j >= 0; j--) {
+                if (a[j].w > a[i].w) {
+                    dp[i] = Math.max(dp[i], dp[j]);
                 }
             }
-            dp[i]++;
+            dp[i] += a[i].h;
             answer = Math.max(answer, dp[i]);
         }
 
