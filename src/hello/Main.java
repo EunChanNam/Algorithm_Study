3febd3af345b786cfad6dc6426f15f8fcbd7fd19
남은찬 <eunchan@nam-eunchan-ui-MacBookAir.local>
2022-12-19 14:09:06 +0900

    Study : 최대부분증가수열(LIS)

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -4,39 +4,32 @@ import java.util.*;
 
 public class Main {
     static int n;
-    static int[] arr;
-
-
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
         n = Integer.parseInt(br.readLine());
-        arr = new int[n];
+        int[] a = new int[n];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            a[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[] answer = new int[n];
-        for (int i = 0; i < n; i++) {
-            int cnt = 0;
-            for (int x = 0; x < n; x++) {
-                if (cnt == arr[i]) {
-                    int t = x;
-                    while (answer[t] != 0) t++;
-                    answer[t] = i + 1;
-                    break;
+        int answer = 1;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        for (int i = 1; i < n; i++) {
+            for (int x = i - 1; x >= 0; x--) {
+                if (a[x] < a[i]) {
+                    dp[i] = Math.max(dp[i], dp[x]);
                 }
-                if (answer[x] == 0) cnt++;
             }
+            dp[i]++;
+            answer = Math.max(answer, dp[i]);
         }
 
-        for (int i = 0; i < n; i++) {
-            if (i == n - 1) bw.write(String.valueOf(answer[i]));
-            else bw.write(answer[i] + " ");
-        }
+        bw.write(String.valueOf(answer));
 
         br.close();
         bw.close();
