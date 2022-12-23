536f3e69906ca1afa789826ea9cf53e96c40dec2
남은찬 <eunchan@nam-eunchan-ui-MacBookAir.local>
2022-12-23 14:37:44 +0900

    백준 2294 동전2

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -10,21 +10,28 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int[] dp = new int[m + 1];
+        int[][] dp = new int[2][m + 1];
+        for (int i = 0; i < m + 1; i++) {
+            dp[0][i] = 200000;
+        }
+
         int[] coin = new int[n];
         for (int i = 0; i < n; i++) {
             coin[i] = Integer.parseInt(br.readLine());
         }
-        dp[0] = 1;
 
+        dp[1][0] = 1;
+        dp[0][0] = 0;
         for (int c : coin) {
             for (int i = c; i < m + 1; i++) {
-                dp[i] += dp[i - c];
+                dp[0][i] = Math.min(dp[0][i], dp[0][i - c] + 1);
+                dp[1][i] += dp[1][i - c];
+                int a =3;
             }
         }
 
-        bw.write(String.valueOf(dp[m]));
-
+        if (dp[1][m] == 0) bw.write("-1");
+        else bw.write(String.valueOf(dp[0][m]));
 
         br.close();
         bw.close();
