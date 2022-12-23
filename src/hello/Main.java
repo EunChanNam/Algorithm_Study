2f5c603c13c3ce992db94d1a7d4e29137a4f55f7
남은찬 <eunchan@nam-eunchan-ui-MacBookAir.local>
2022-12-23 12:55:20 +0900

    백준 2775 부녀회장이 될테야

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -9,27 +9,23 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n];
+
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-
-        int[] plus = {1, 2, 3};
-        int[] dp = new int[1000001];
-        dp[0] = 1;
-
-        for (int i = 1; i < 1000001; i++) {
-            for (int p : plus) {
-                if (i >= p) {
-                    dp[i] = (dp[i] + dp[i - p]) % 1000000009;
+            int a = Integer.parseInt(br.readLine());
+            int b = Integer.parseInt(br.readLine());
+            int[][] dp = new int[1000][b + 1];
+            for (int k = 0; k < b + 1; k++) {
+                dp[0][k] = k;
+            }
+            for (int y = 1; y < a + 1; y++) {
+                for (int x = 1; x < b + 1; x++) {
+                    dp[y][x] = dp[y][x - 1] + dp[y - 1][x];
                 }
             }
+            bw.write(String.valueOf(dp[a][b]));
+            bw.newLine();
         }
 
-        for (int i = 0; i < n; i++) {
-            bw.write(String.valueOf(dp[arr[i]]));
-            if (i != n - 1) bw.newLine();
-        }
 
         br.close();
         bw.close();
