6ed7e0cddb8079a473dd226daa7cb4a3d1153f55
남은찬 <eunchan@nam-eunchan-ui-MacBookAir.local>
2022-12-23 14:17:41 +0900

    백준 2293 동전1

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -9,22 +9,21 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
-
+        int m = Integer.parseInt(st.nextToken());
+        int[] dp = new int[m + 1];
+        int[] coin = new int[n];
         for (int i = 0; i < n; i++) {
-            int a = Integer.parseInt(br.readLine());
-            int b = Integer.parseInt(br.readLine());
-            int[][] dp = new int[1000][b + 1];
-            for (int k = 0; k < b + 1; k++) {
-                dp[0][k] = k;
-            }
-            for (int y = 1; y < a + 1; y++) {
-                for (int x = 1; x < b + 1; x++) {
-                    dp[y][x] = dp[y][x - 1] + dp[y - 1][x];
-                }
-            }
-            bw.write(String.valueOf(dp[a][b]));
-            bw.newLine();
+            coin[i] = Integer.parseInt(br.readLine());
         }
+        dp[0] = 1;
+
+        for (int c : coin) {
+            for (int i = c; i < m + 1; i++) {
+                dp[i] += dp[i - c];
+            }
+        }
+
+        bw.write(String.valueOf(dp[m]));
 
 
         br.close();
