08a9b47ae36a6d6ec18b8e24ff66f0e54edf23aa
남은찬 <eunchan@nam-eunchan-ui-MacBookAir.local>
2022-12-24 20:35:50 +0900

    백준 2206 벽 부수고 이동하기

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -6,67 +6,64 @@ public class Main {
     static int n;
     static int m;
     static int[][] map;
-    static int[][] visit;
     static int[][] dir = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+    static int[][][] visit;
     static class Point {
-        int y; int x;
-        public Point(int y, int x){
+        int y; int x; int b; int level;
+        public Point(int y, int x, int b, int level){
             this.y = y;
             this.x = x;
+            this.b = b;
+            this.level = level;
         }
     }
+    static int answer = 1000000000;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        int T = Integer.parseInt(br.readLine());
-        for (int i = 0; i < T; i++) {
-            st = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(st.nextToken());
-            m = Integer.parseInt(st.nextToken());
-            map = new int[m][n];
-            int k = Integer.parseInt(st.nextToken());
-            for (int j = 0; j < k; j++) {
-                st = new StringTokenizer(br.readLine());
-                int dx = Integer.parseInt(st.nextToken());
-                int dy = Integer.parseInt(st.nextToken());
-                map[dy][dx] = 1;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        visit = new int[2][n][m];
+        map = new int[n][m];
+        for (int y = 0; y < n; y++) {
+            String s = br.readLine();
+            for (int x = 0; x < s.length(); x++) {
+                char ch = s.charAt(x);
+                map[y][x] = ch - '0';
             }
-
-            int cnt = 0;
-            visit = new int[m][n];
-            for (int y = 0; y < m; y++) {
-                for (int x = 0; x < n; x++) {
-                    if (map[y][x] == 1) {
-                        if (visit[y][x] == 1) continue;
-                        visit[y][x] = 1;
-                        bfs(y, x);
-                        cnt++;
-                    }
-                }
-            }
-            bw.write(String.valueOf(cnt));
-            if (i != T - 1) bw.newLine();
         }
+
+        bfs();
+        if (answer == 1000000000) answer = -1;
+        bw.write(String.valueOf(answer));
 
         br.close();
         bw.close();
     }
 
-    static void bfs(int y, int x) {
+    static void bfs() {
         Queue<Point> que = new LinkedList<>();
-        que.offer(new Point(y, x));
+        que.offer(new Point(0, 0, 0, 1));
+        visit[0][0][0] = 1;
+        visit[1][0][0] = 1;
 
         while (!que.isEmpty()) {
             Point p = que.poll();
+            if (p.y == n - 1 && p.x == m - 1) answer = Math.min(answer, p.level);
             for (int i = 0; i < 4; i++) {
                 int ny = p.y + dir[i][0];
                 int nx = p.x + dir[i][1];
-                if (ny < 0 || nx < 0 || ny >= m || nx >= n) continue;
-                if (visit[ny][nx] == 1) continue;
-                if (map[ny][nx] == 0) continue;
-                visit[ny][nx] = 1;
-                que.offer(new Point(ny, nx));
+                if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+                if (visit[p.b][ny][nx] == 1) continue;
+                if (map[ny][nx] == 1 && p.b == 1) continue;
+                if (map[ny][nx] == 1 && p.b == 0) {
+                    visit[1][ny][nx] = 1;
+                    que.offer(new Point(ny, nx, 1, p.level + 1));
+                } else {
+                    visit[p.b][ny][nx] = 1;
+                    que.offer(new Point(ny, nx, p.b, p.level + 1));
+                }
             }
         }
     }
