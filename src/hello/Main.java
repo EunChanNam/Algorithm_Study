6a3b41633587271354ea179dc35fb3de86749344
EunChanNam <namhm23@gmail.com>
2022-12-27 02:07:37 +0900

    백준 5557 1학년

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -4,73 +4,40 @@ import java.util.*;
 
 public class Main {
     static int n;
-    static char[][] map1;
-    static char[][] map2;
-    static int[][] visit1;
-    static int[][] visit2;
-    static int cnt1 = 0;
-    static int cnt2 = 0;
-    static int[][] dir = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
 
-    static void bfs(int y, int x, char[][] arr, int[][] visit, char now) {
-        Queue<Node> que = new LinkedList<>();
-        que.offer(new Node(y, x));
-        visit[y][x] = 1;
-
-        while (!que.isEmpty()) {
-            Node p = que.poll();
-            for (int i = 0; i < 4; i++) {
-                int ny = p.y + dir[i][0];
-                int nx = p.x + dir[i][1];
-                if (ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
-                if (visit[ny][nx] == 1) continue;
-                if (arr[ny][nx] != now) continue;
-                visit[ny][nx] = 1;
-                que.offer(new Node(ny, nx));
-            }
-        }
-    }
-    static class Node{
-        int y; int x;
-        public Node(int y, int x){
-            this.y = y;
-            this.x = x;
-        }
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
         n = Integer.parseInt(br.readLine());
-        map1 = new char[n][n];
-        map2 = new char[n][n];
-        visit1 = new int[n][n];
-        visit2 = new int[n][n];
-
-        for (int y = 0; y < n; y++) {
-            String s = br.readLine();
-            for (int x = 0; x < n; x++) {
-                char ch = s.charAt(x);
-                map1[y][x] = ch;
-                if (ch == 'G') map2[y][x] = 'R';
-                else map2[y][x] = ch;
-            }
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int y = 0; y < n; y++) {
-            for (int x = 0; x < n; x++) {
-                if (visit1[y][x] == 0) {
-                    bfs(y, x, map1, visit1, map1[y][x]);
-                    cnt1++;
-                }
-                if (visit2[y][x] == 0) {
-                    bfs(y, x, map2, visit2, map2[y][x]);
-                    cnt2++;
+        int goal = arr[n - 1];
+        long[][] dp = new long[n][21];
+
+        dp[0][arr[0]] = 1;
+
+        for (int y = 1; y < n - 1; y++) {
+            for (int x = 0; x <= 20; x++) {
+                if (dp[y - 1][x] != 0) {
+                    int plus = x + arr[y];
+                    int minus = x - arr[y];
+                    if (plus <= 20) {
+                        dp[y][plus] += dp[y - 1][x];
+                    }
+                    if (minus >= 0) {
+                        dp[y][minus] += dp[y - 1][x];
+                    }
                 }
             }
         }
 
-        bw.write(cnt1 + " " + cnt2);
+        bw.write(String.valueOf(dp[n - 2][goal]));
+
 
         br.close();
         bw.close();
