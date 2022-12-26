507d6bca09462fff1d8340aa8e1867079029fac3
남은찬 <eunchan@nam-eunchan-ui-MacBookAir.local>
2022-12-26 14:30:22 +0900

    백준 12100 2048(Easy)

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -4,51 +4,140 @@ import java.util.*;
 
 public class Main {
     static int n;
-    static Node[] arr;
-    static int goal;
-    static int start;
-    static class Node{
-        int dis; int val;
-        public Node(int dis, int val){
-            this.dis = dis;
-            this.val = val;
+    static int max = Integer.MIN_VALUE;
+    static void dfs(int level, int[][] map, int k) {
+        if (level == 5) return;
+        int[][] arr = new int[n][n];
+        for (int y = 0; y < n; y++) {
+            for (int x = 0; x < n; x++) {
+                arr[y][x] = map[y][x];
+            }
+        }
+
+        move(arr, k);
+        for (int i = 1; i <= 4; i++) {
+            dfs(level + 1, arr, i);
         }
     }
+
+    static void move(int[][] arr, int k) {
+        if (k == 1) {
+            for (int x = 0; x < n; x++) {
+                Stack<Integer> stack = new Stack<>();
+                boolean db = false;
+                for (int y = 0; y < n; y++) {
+                    if (!stack.isEmpty() && stack.peek() == arr[y][x] && !db) {
+                        int p = stack.pop();
+                        stack.push(p + arr[y][x]);
+                        db = true;
+                    } else if (arr[y][x] != 0) {
+                        stack.push(arr[y][x]);
+                        db = false;
+                    }
+                }
+                int t = 0;
+                for (int p : stack) {
+                    max = Math.max(max, p);
+                    arr[t][x] = p;
+                    t++;
+                }
+                for (int y = t; y < n; y++) {
+                    arr[y][x] = 0;
+                }
+            }
+        } else if (k == 2) {
+            for (int y = 0; y < n; y++) {
+                Stack<Integer> stack = new Stack<>();
+                boolean db = false;
+                for (int x = 0; x < n; x++) {
+                    if (!stack.isEmpty() && stack.peek() == arr[y][x] && !db) {
+                        int p = stack.pop();
+                        stack.push(p + arr[y][x]);
+                        db = true;
+                    } else if (arr[y][x] != 0) {
+                        stack.push(arr[y][x]);
+                        db = false;
+                    }
+                }
+                int t = 0;
+                for (int p : stack) {
+                    max = Math.max(max, p);
+                    arr[y][t] = p;
+                    t++;
+                }
+                for (int x = t; x < n; x++) {
+                    arr[y][x] = 0;
+                }
+            }
+        } else if (k == 3) {
+            for (int x = 0; x < n; x++) {
+                Stack<Integer> stack = new Stack<>();
+                boolean db = false;
+                for (int y = n - 1; y >= 0; y--) {
+                    if (!stack.isEmpty() && stack.peek() == arr[y][x] && !db) {
+                        int p = stack.pop();
+                        stack.push(p + arr[y][x]);
+                        db = true;
+                    } else if (arr[y][x] != 0) {
+                        stack.push(arr[y][x]);
+                        db = false;
+                    }
+                }
+                int t = n - 1;
+                for (int p : stack) {
+                    max = Math.max(max, p);
+                    arr[t][x] = p;
+                    t--;
+                }
+                for (int y = t; y >= 0; y--) {
+                    arr[y][x] = 0;
+                }
+            }
+        } else if (k == 4) {
+            for (int y = 0; y < n; y++) {
+                Stack<Integer> stack = new Stack<>();
+                boolean db = true;
+                for (int x = n - 1; x >= 0; x--) {
+                    if (!stack.isEmpty() && stack.peek() == arr[y][x] && !db) {
+                        int p = stack.pop();
+                        stack.push(p + arr[y][x]);
+                        db = true;
+                    } else if (arr[y][x] != 0) {
+                        stack.push(arr[y][x]);
+                        db = false;
+                    }
+                }
+                int t = n - 1;
+                for (int p : stack) {
+                    max = Math.max(max, p);
+                    arr[y][t] = p;
+                    t--;
+                }
+                for (int x = t; x >= 0; x--) {
+                    arr[y][x] = 0;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
         n = Integer.parseInt(br.readLine());
-        arr = new Node[n];
-        for (int i = 0; i < n; i++) {
+        int[][] map = new int[n][n];
+        for (int y = 0; y < n; y++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            arr[i] = new Node(a, b);
-        }
-        st = new StringTokenizer(br.readLine());
-        goal = Integer.parseInt(st.nextToken());
-        start = Integer.parseInt(st.nextToken());
-
-        Arrays.sort(arr, (a, b) -> Integer.compare(a.dis, b.dis));
-        PriorityQueue<Node> que = new PriorityQueue<>((a, b) -> Integer.compare(b.val, a.val));
-
-        int answer = 0;
-        int k = start;
-        int x = 0;
-        while (k < goal) {
-            for (; x < n; x++) {
-                if (k < arr[x].dis) break;
-                que.offer(arr[x]);
+            for (int x = 0; x < n; x++) {
+                map[y][x] = Integer.parseInt(st.nextToken());
             }
-            if (que.isEmpty()) break;
-            Node p = que.poll();
-            k += p.val;
-            answer++;
         }
 
-        if (k < goal) answer = -1;
-        bw.write(String.valueOf(answer));
+        for (int i = 1; i <= 4; i++) {
+            dfs(0, map, i);
+        }
+
+        bw.write(String.valueOf(max));
 
         br.close();
         bw.close();
