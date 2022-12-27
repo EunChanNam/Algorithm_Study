a78d15869f0136882b47dd3dc2c3ce88e5a0b07f
EunChanNam <namhm23@gmail.com>
2022-12-27 14:06:55 +0900

    프로그래머스 43238 입국심사

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -3,41 +3,36 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n;
+    public long solution(int n, int[] times) {
+        long answer = Long.MAX_VALUE;
 
+        Arrays.sort(times);
+        int len = times.length;
+        long last = times[len - 1];
+
+        long max = last * n;
+        long lt = 0;
+        long rt = max;
+
+        while(lt <= rt){
+            long m = (lt + rt) / 2;
+            long cnt = 0;
+            for (int i=0; i < len; i++){
+                cnt += m / times[i];
+            }
+            if (cnt < n) lt = m + 1;
+            else if (cnt >= n) {
+                rt = m - 1;
+                answer = Math.min(answer, m);
+            }
+        }
+
+        return answer;
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
-        n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int goal = arr[n - 1];
-        long[][] dp = new long[n][21];
-
-        dp[0][arr[0]] = 1;
-
-        for (int y = 1; y < n - 1; y++) {
-            for (int x = 0; x <= 20; x++) {
-                if (dp[y - 1][x] != 0) {
-                    int plus = x + arr[y];
-                    int minus = x - arr[y];
-                    if (plus <= 20) {
-                        dp[y][plus] += dp[y - 1][x];
-                    }
-                    if (minus >= 0) {
-                        dp[y][minus] += dp[y - 1][x];
-                    }
-                }
-            }
-        }
-
-        bw.write(String.valueOf(dp[n - 2][goal]));
-
 
         br.close();
         bw.close();
