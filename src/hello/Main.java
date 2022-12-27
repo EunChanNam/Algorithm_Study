e78700fa614e3d68051a6a3dc125c0b963c78073
EunChanNam <namhm23@gmail.com>
2022-12-27 16:39:22 +0900

    백준 1937 욕심쟁이 판다

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -3,60 +3,54 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    //BFS를 도착지 기준으로 돌리면 모든 시작점으로 부터의 최소거리를 한번에 구할 수 있다.
+    //DP를 기본으로 풀면서 그리디한 우선순위큐를 적용해서 시간복잡도를 줄이며 풀었다.
     static int n;
-    static Map<Integer, Integer> map = new HashMap<>();
+    static int[][] dir = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+
     static class Node{
-        int k; int level;
-        public Node(int k, int level){
-            this.k = k;
-            this.level = level;
+        int y; int x; int val;
+        public Node(int y, int x, int val){
+            this.y = y;
+            this.x = x;
+            this.val = val;
         }
     }
-    static List<List<Integer>> list = new ArrayList<>();
-    public int[] solution(int m, int[][] r, int[] s, int d) {
-        n = m;
-        int[] answer = new int [s.length];
 
-        for (int i=0; i <= n; i++){
-            list.add(new ArrayList<>());
-        }
-        for (int i=0; i < r.length; i++){
-            list.get(r[i][0]).add(r[i][1]);
-            list.get(r[i][1]).add(r[i][0]);
-        }
-
-        int[] dis = bfs(d);
-        for (int i=0; i < s.length; i++){
-            int t = dis[s[i]];
-            if (t == 0 && s[i] != d) t = -1;
-            answer[i] = t;
-        }
-        return answer;
-    }
-    static int[] bfs(int end){
-        Queue<Node> que = new LinkedList<>();
-        int[] dis = new int[n + 1];
-        int[] visit = new int[n + 1];
-        visit[end] = 1;
-        que.offer(new Node(end, 0));
-
-        while(!que.isEmpty()){
-            Node p = que.poll();
-            dis[p.k] = p.level;
-            for (int i=0; i < list.get(p.k).size(); i++){
-                int next = list.get(p.k).get(i);
-                if (visit[next] == 1) continue;
-                visit[next] = 1;
-                que.offer(new Node(next, p.level + 1));
-            }
-        }
-        return dis;
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        int[][] map = new int[n][n];
+        int[][] dp = new int[n][n];
+        PriorityQueue<Node> que = new PriorityQueue<>(Comparator.comparingInt(a -> a.val));
+
+        for (int y = 0; y < n; y++) {
+            st = new StringTokenizer(br.readLine());
+            for (int x = 0; x < n; x++) {
+                map[y][x] = Integer.parseInt(st.nextToken());
+                dp[y][x] = 1;
+                que.offer(new Node(y, x, map[y][x]));
+            }
+        }
+
+
+
+
+        int answer = 0;
+        while (!que.isEmpty()) {
+            Node p = que.poll();
+            for (int i = 0; i < 4; i++) {
+                int ny = p.y + dir[i][0];
+                int nx = p.x + dir[i][1];
+                if (ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
+                if (map[ny][nx] >= map[p.y][p.x]) continue;
+                dp[p.y][p.x] = Math.max(dp[p.y][p.x], dp[ny][nx] + 1);
+            }
+            answer = Math.max(answer, dp[p.y][p.x]);
+        }
+
+        bw.write(String.valueOf(answer));
 
         br.close();
         bw.close();
