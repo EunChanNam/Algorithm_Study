6038266ed550dc54e2d37ce246e41dbb4ca57c15
EunChanNam <namhm23@gmail.com>
2022-12-29 14:12:42 +0900

    백준 15663 N과 M(9)

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -5,21 +5,29 @@ import java.util.*;
 public class Main {
     static int n;
     static int m;
+    static int[] arr;
     static Stack<Integer> stack = new Stack<>();
+    static int[] visit;
+    static Set<String> set = new HashSet<>();
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    static void dfs(int level, int start) throws IOException {
+    static void dfs(int level, String str) throws IOException {
         if (level == m) {
+            if (set.contains(str)) return;
             for (int a : stack) {
                 bw.write(a + " ");
             }
             bw.newLine();
+            set.add(str);
             return;
         }
-        for (int i = start; i <= n; i++) {
-            stack.push(i);
-            dfs(level + 1, i);
+        for (int i = 0; i < n; i++) {
+            if (visit[i] == 1) continue;
+            visit[i] = 1;
+            stack.push(arr[i]);
+            dfs(level + 1, str + arr[i]);
             stack.pop();
+            visit[i] = 0;
         }
     }
 
@@ -29,8 +37,15 @@ public class Main {
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        visit = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
 
-        dfs(0, 1);
+        dfs(0, "");
 
         br.close();
         bw.close();
