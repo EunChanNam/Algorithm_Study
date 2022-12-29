b636f06d6224f6d7a3e1476304b901e11401ad43
EunChanNam <namhm23@gmail.com>
2022-12-29 13:03:15 +0900

    백준 1759 암호 만들기

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -3,32 +3,41 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int n;
+    static int m;
+    static char[] w;
+    static int[] visit;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static void dfs(int level, int a, int b, String str, int start) throws IOException {
+        if (level == n) {
+            if (a < 1 || b < 2) return;
+            bw.write(str);
+            bw.newLine();
+            return;
+        }
+        for (int i = start; i < m; i++) {
+            if (w[i] == 'a' || w[i] == 'e' || w[i] == 'i' || w[i] == 'o' || w[i] == 'u') {
+                dfs(level + 1, a + 1, b, str + w[i], i + 1);
+            } else dfs(level + 1, a, b + 1, str + w[i], i + 1);
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        String[] arr1 = new String[n];
-        String[] arr2 = new String[m];
-
-        for (int i = 0; i < n; i++) {
-            arr1[i] = br.readLine();
-        }
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        w = new char[m];
         for (int i = 0; i < m; i++) {
-            arr2[i] = br.readLine();
+            w[i] = st.nextToken().charAt(0);
         }
+        Arrays.sort(w);
+        visit = new int[m];
 
-        int answer = 0;
-        for (int y = 0; y < m; y++) {
-            for (int x = 0; x < n; x++) {
-                if (arr2[y].equals(arr1[x])) answer++;
-            }
-        }
-
-        bw.write(String.valueOf(answer));
+        dfs(0, 0, 0, "", 0);
 
         br.close();
         bw.close();
