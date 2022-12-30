38185ea1b1583fcd2029f79b59e6c5e0a9a89292
EunChanNam <namhm23@gmail.com>
2022-12-31 01:07:22 +0900

    백준 2156 포도주 시식

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -1,6 +1,5 @@
 package hello;
 import java.io.*;
-import java.time.Year;
 import java.util.*;
 
 public class Main {
@@ -12,21 +11,29 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         n = Integer.parseInt(st.nextToken());
-        int[] dp = new int[n + 1];
+        int[][] dp = new int[2][n + 1];
         int[] arr = new int[n + 1];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
 
         for (int i = 1; i <= n; i++) {
-            for (int x = i; x >= 1; x--) {
-                int temp = Math.abs(arr[i] - arr[x]) + dp[x - 1];
-                dp[i] = Math.max(temp, dp[i]);
-            }
+            arr[i] = Integer.parseInt(br.readLine());
         }
+        dp[0][1] = arr[1];
 
-        bw.write(String.valueOf(dp[n]));
+        int answer = 0;
+        for (int i = 2; i <= n; i++) {
+            //선택
+            int max = Math.max(arr[i - 1] + dp[1][i - 2], dp[1][i - 1]); //이전 선택 vs 이전 선택 X
+            dp[0][i] = max + arr[i];
+            //노 선택
+            max = Math.max(dp[0][i - 1], dp[1][i - 1]);
+            dp[1][i] = max;
+
+            answer = Math.max(answer, dp[0][i]);
+            answer = Math.max(answer, dp[1][i]);
+        }
+        if (n == 1) answer = arr[1];
+
+        bw.write(String.valueOf(answer));
 
         br.close();
         bw.close();
