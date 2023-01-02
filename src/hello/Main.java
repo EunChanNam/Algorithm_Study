1a9d4c86e7f079babf1e2eccaae6fd5600659b6e
EunChanNam <namhm23@gmail.com>
2023-01-02 15:41:17 +0900

    프로그래머스 12978 배달

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -3,21 +3,49 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public int solution(int n, int k, int[] e) {
-        int answer = e.length;
+    static int m;
+    static List<List<Node>> list = new ArrayList<>();
+    static class Node{
+        int k; int val;
+        public Node(int k, int val){
+            this.k = k;
+            this.val = val;
+        }
+    }
+    static int[] visit;
+    static int[] ret;
+    static void dfs(int now, int sum){
+        for (int i=0; i < list.get(now).size(); i++){
+            List<Node> nows = list.get(now);
+            Node next = nows.get(i);
+            if (visit[next.k] == 1) continue;
+            if ((sum + next.val) > m) continue;
+            visit[next.k] = 1;
+            ret[next.k] = 1;
+            dfs(next.k, sum + next.val);
+            visit[next.k] = 0;
+        }
+    }
+    public int solution(int n, int[][] road, int M) {
+        int answer = 0;
+        m = M;
+        visit = new int[n + 1];
+        ret = new int[n + 1];
 
-        PriorityQueue<Integer> que = new PriorityQueue<>();
-        for (int i=0; i < e.length; i++){
-            que.offer(e[i]);
+        for (int i=0; i <= n; i++){
+            list.add(new ArrayList<>());
+        }
 
-            if (que.size() > k){
-                n -= que.poll();
-            }
+        for (int y=0; y < road.length; y++){
+            list.get(road[y][0]).add(new Node(road[y][1], road[y][2]));
+            list.get(road[y][1]).add(new Node(road[y][0], road[y][2]));
+        }
 
-            if (n < 0) {
-                answer = i;
-                break;
-            }
+        visit[1] = 1;
+        ret[1] = 1;
+        dfs(1, 0);
+        for (int i=0; i <= n; i++){
+            if (ret[i] == 1) answer++;
         }
 
         return answer;
