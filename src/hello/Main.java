c788f7bb43f8b0d74f6b9c7fce179dc65de34e5b
EunChanNam <namhm23@gmail.com>
2023-01-03 15:30:13 +0900

    백준 2589 보물섬

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -4,66 +4,61 @@ import java.util.*;
 
 public class Main {
     static int n;
-    static int[][] map;
-    static Stack<Node> stack = new Stack<>();
-    static List<Node> list = new ArrayList<>();
-    static int answer = Integer.MAX_VALUE;
+    static int m;
+    static char[][] map;
     static int[][] dir = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+    static int max = Integer.MIN_VALUE;
 
-    static class Node{
-        int y; int x; int val;
-        public Node(int y, int x, int val){
-            this.y = y;
-            this.x = x;
-            this.val = val;
+    static void bfs(int y, int x) {
+        int[][] visit = new int[n][m];
+        visit[y][x] = 1;
+        Queue<Node> que = new LinkedList<>();
+        que.offer(new Node(y, x, 0));
+
+        while (!que.isEmpty()) {
+            Node p = que.poll();
+            max = Math.max(max, p.level);
+            for (int i = 0; i < 4; i++) {
+                int ny = p.y + dir[i][0];
+                int nx = p.x + dir[i][1];
+                if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+                if (visit[ny][nx] == 1) continue;
+                if (map[ny][nx] == 'W') continue;
+                visit[ny][nx] = 1;
+                que.offer(new Node(ny, nx, p.level + 1));
+            }
         }
     }
-    static void dfs(int level, int start) {
-        if (level == 3) {
-            int[][] visit = new int[n][n];
-            int sum = 0;
-            for (Node p : stack) {
-                if (visit[p.y][p.x] == 1) return;
-                visit[p.y][p.x] = 1;
-                sum += map[p.y][p.x];
-                for (int i = 0; i < 4; i++) {
-                    int ny = p.y + dir[i][0];
-                    int nx = p.x + dir[i][1];
-                    if (visit[ny][nx] == 1) return;
-                    visit[ny][nx] = 1;
-                    sum += map[ny][nx];
-                }
-            }
-            answer = Math.min(answer, sum);
-            return;
-        }
-        for (int i = start; i < list.size(); i++) {
-            stack.push(list.get(i));
-            dfs(level + 1, i + 1);
-            stack.pop();
+    static class Node{
+        int y; int x; int level;
+        public Node(int y, int x, int level){
+            this.y = y;
+            this.x = x;
+            this.level = level;
         }
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
-        n = Integer.parseInt(br.readLine());
-        map = new int[n][n];
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new char[n][m];
         for (int y = 0; y < n; y++) {
-            st = new StringTokenizer(br.readLine());
-            for (int x = 0; x < n; x++) {
-                map[y][x] = Integer.parseInt(st.nextToken());
-            }
-        }
-        for (int y = 1; y < n - 1; y++) {
-            for (int x = 1; x < n - 1; x++) {
-                list.add(new Node(y, x, map[y][x]));
+            String str = br.readLine();
+            for (int x = 0; x < m; x++) {
+                map[y][x] = str.charAt(x);
             }
         }
 
-        dfs(0, 0);
+        for (int y = 0; y < n; y++) {
+            for (int x = 0; x < m; x++) {
+                if (map[y][x] == 'L') bfs(y, x);
+            }
+        }
 
-        bw.write(String.valueOf(answer));
+        bw.write(String.valueOf(max));
 
         br.close();
         bw.close();
