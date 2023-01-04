2d30d699fae03e94892e722e4737878d5fa8e606
EunChanNam <namhm23@gmail.com>
2023-01-04 16:33:51 +0900

    백준 3151 합이 0

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -4,61 +4,56 @@ import java.util.*;
 
 public class Main {
     static int n;
-    static int m;
-    static char[][] map;
-    static int[][] dir = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
-    static int max = Integer.MIN_VALUE;
+    static int[] arr;
+    static long answer = 0;
 
-    static void bfs(int y, int x) {
-        int[][] visit = new int[n][m];
-        visit[y][x] = 1;
-        Queue<Node> que = new LinkedList<>();
-        que.offer(new Node(y, x, 0));
 
-        while (!que.isEmpty()) {
-            Node p = que.poll();
-            max = Math.max(max, p.level);
-            for (int i = 0; i < 4; i++) {
-                int ny = p.y + dir[i][0];
-                int nx = p.x + dir[i][1];
-                if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-                if (visit[ny][nx] == 1) continue;
-                if (map[ny][nx] == 'W') continue;
-                visit[ny][nx] = 1;
-                que.offer(new Node(ny, nx, p.level + 1));
-            }
-        }
-    }
-    static class Node{
-        int y; int x; int level;
-        public Node(int y, int x, int level){
-            this.y = y;
-            this.x = x;
-            this.level = level;
-        }
+    static int comb(int k) {
+        return k * (k - 1) / 2;
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
         st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        map = new char[n][m];
-        for (int y = 0; y < n; y++) {
-            String str = br.readLine();
-            for (int x = 0; x < m; x++) {
-                map[y][x] = str.charAt(x);
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n - 2; i++) {
+            if (arr[i] > 0) break;
+            int lt = i + 1;
+            int rt = n - 1;
+            while (lt < rt) {
+                int sum = arr[i] + arr[lt] + arr[rt];
+                if (sum == 0) {
+                    if (arr[lt] == arr[rt]) {
+                        answer += comb(rt - lt + 1);
+                        break;
+                    }
+                    int s = 1;
+                    int e = 1;
+                    while (arr[lt] == arr[lt + 1] && lt < rt - 1) {
+                        lt++;
+                        s++;
+                    }
+                    while (arr[rt] == arr[rt - 1] && rt > lt + 1) {
+                        rt--;
+                        e++;
+                    }
+                    lt++; rt--;
+                    answer += (long)s * e;
+                }
+                else if (sum < 0) {
+                    lt++;
+                } else rt--;
             }
         }
 
-        for (int y = 0; y < n; y++) {
-            for (int x = 0; x < m; x++) {
-                if (map[y][x] == 'L') bfs(y, x);
-            }
-        }
-
-        bw.write(String.valueOf(max));
+        bw.write(String.valueOf(answer));
 
         br.close();
         bw.close();
