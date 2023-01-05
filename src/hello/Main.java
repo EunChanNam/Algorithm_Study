dfce0ade4e4562e0530248384cf6aaf79d3f268e
EunChanNam <namhm23@gmail.com>
2023-01-05 17:09:05 +0900

    백준 2798 블랙잭

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -4,56 +4,41 @@ import java.util.*;
 
 public class Main {
     static int n;
-    static int[] arr;
-    static long answer = 0;
+    static int m;
+    static int[] a;
+    static int max = Integer.MIN_VALUE;
+    static Stack<Integer> stack = new Stack<>();
 
-
-    static int comb(int k) {
-        return k * (k - 1) / 2;
+    static void dfs(int level, int start) {
+        if (level == 3) {
+            int sum = stack.stream().reduce(Integer::sum).get();
+            if (sum > m) return;
+            max = Math.max(max, sum);
+            return;
+        }
+        for (int i = start; i < n; i++) {
+            stack.push(a[i]);
+            dfs(level + 1, i + 1);
+            stack.pop();
+        }
     }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
-        n = Integer.parseInt(br.readLine());
-        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        a = new int[n];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(arr);
-
-        for (int i = 0; i < n - 2; i++) {
-            if (arr[i] > 0) break;
-            int lt = i + 1;
-            int rt = n - 1;
-            while (lt < rt) {
-                int sum = arr[i] + arr[lt] + arr[rt];
-                if (sum == 0) {
-                    if (arr[lt] == arr[rt]) {
-                        answer += comb(rt - lt + 1);
-                        break;
-                    }
-                    int s = 1;
-                    int e = 1;
-                    while (arr[lt] == arr[lt + 1] && lt < rt - 1) {
-                        lt++;
-                        s++;
-                    }
-                    while (arr[rt] == arr[rt - 1] && rt > lt + 1) {
-                        rt--;
-                        e++;
-                    }
-                    lt++; rt--;
-                    answer += (long)s * e;
-                }
-                else if (sum < 0) {
-                    lt++;
-                } else rt--;
-            }
+            a[i] = Integer.parseInt(st.nextToken());
         }
 
-        bw.write(String.valueOf(answer));
+        dfs(0, 0);
+
+        bw.write(String.valueOf(max));
 
         br.close();
         bw.close();
