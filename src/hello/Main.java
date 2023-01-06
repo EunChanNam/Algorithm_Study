593abb069b6acb0cf8c42b548d45d0bfec4888fc
EunChanNam <namhm23@gmail.com>
2023-01-06 12:29:34 +0900

    백준 1062 가르침

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -4,51 +4,59 @@ import java.util.*;
 
 public class Main {
     static int n;
+    static int k;
+    static String[] s;
+    static int[] dat = new int[150];
+    static int answer = 0;
+
+    static void dfs(int level, int start) {
+        if (level == k) {
+            int cnt = 0;
+            for (int i = 0; i < n; i++) {
+                boolean flag = true;
+                for (char ch : s[i].toCharArray()) {
+                    if (dat[ch] == 0) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) cnt++;
+            }
+            answer = Math.max(answer, cnt);
+            return;
+        }
+        for (int i = start; i <= 'z'; i++) {
+            if (dat[i] == 1) continue;
+            dat[i] = 1;
+            dfs(level + 1, i + 1);
+            dat[i] = 0;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
         st = new StringTokenizer(br.readLine());
-        int[] coin = {1, 5, 10, 25};
-        int[] coins = new int[4];
-        for (int i = 0; i < 5; i++) {
-            if (i == 0) n = Integer.parseInt(st.nextToken());
-            else coins[i - 1] = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        s = new String[n];
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            if (str.equals("antatica")) s[i] = "";
+            else s[i] = str;
         }
+        if (k < 5) {
+            bw.write("0");
+            br.close();
+            bw.close();
+            return;
+        }
+        k -= 5;
+        dat['a'] = 1; dat['c'] = 1; dat['i'] = 1; dat['n'] = 1; dat['t'] = 1;
 
-        int[] cnt = new int[n + 1];
-        int[] sum = new int[n + 1];
-        int[][] dp = new int[4][n + 1];
-        for (int i = 1; i <= n; i++) {
-            int max = -1;
-            int maxIndex = -1;
-            int selectedX = -1;
-            boolean flag = false;
-            for (int x = 0; x < 4; x++) {
-                if (coin[x] > i) break;
-                int pre = i - coin[x];
-                if (dp[x][pre] + 1 > coins[x]) continue;
-                if (sum[pre] + coin[x] != i) continue;
-                if (cnt[pre] > max){
-                    max = cnt[pre];
-                    maxIndex = pre;
-                    selectedX = x;
-                }
-                flag = true;
-            }
-            if (flag) {
-                cnt[i] = cnt[maxIndex] + 1;
-                sum[i] = sum[maxIndex] + coin[selectedX];
-                for (int x = 0; x < 4; x++) {
-                    dp[x][i] = dp[x][maxIndex];
-                    if (x == selectedX) dp[x][i]++;
-                }
-            }
-        }
-        for (int i = 0; i < 4; i++) {
-            bw.write(dp[i][n] + " ");
-        }
+        dfs(0, 'a');
+        bw.write(String.valueOf(answer));
 
         br.close();
         bw.close();
