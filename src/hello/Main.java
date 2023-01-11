42fcdb9532fc9dc0654d16bc53e9f43dc9e695ce
EunChanNam <namhm23@gmail.com>
2023-01-12 02:00:16 +0900

    백준 2138 전구와 스위치

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -4,9 +4,9 @@ import java.util.*;
 
 public class Main {
     static int n;
-    static int m;
-    static int[][] map;
-    static int[] plus;
+    static int[] a;
+    static int[] temp;
+    static int[] b;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,49 +14,54 @@ public class Main {
         StringTokenizer st;
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        map = new int[n][n];
-        for (int y = 0; y < n; y++) {
-            for (int x = 0; x < n; x++) {
-                map[y][x] = 1;
-            }
+        a = new int[n];
+        temp = new int[n];
+        b = new int[n];
+        String str = br.readLine();
+        for (int i=0; i < n; i++) {
+            int k = str.charAt(i) - '0';
+            a[i] = k;
+            temp[i] = k;
         }
-        plus = new int[2 * n - 1];
-        for (int y = 0; y < m; y++) {
-            st = new StringTokenizer(br.readLine());
-            int t = 0;
-            for (int x = 0; x < 3; x++) {
-                int p = Integer.parseInt(st.nextToken());
-                for (int k = 0; k < p; k++) {
-                    plus[t++] += x;
-                }
-            }
+        str = br.readLine();
+        for (int i=0; i < n; i++) {
+            int k = str.charAt(i) - '0';
+            b[i] = k;
         }
 
-        int dy = n - 1;
-        int dx = 0;
-        for (int i = 0; i < 2 * n - 1; i++) {
-            map[dy][dx] += plus[i];
-            if (dy != 0) dy--;
-            else dx++;
-        }
+        //키고 시작할 때
+        change(a, 0);
+        change(a, 1);
+        int cnt1 = run(a, 1);
+        int cnt2 = run(temp, 0);
+        int answer = 0;
+        if (cnt1 == -1 && cnt2 == -1) answer = -1;
+        else if (cnt1 == -1) answer = cnt2;
+        else if (cnt2 == -1) answer = cnt1;
+        else answer = Math.min(cnt1, cnt2);
 
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < n; j++) {
-                map[i][j] = Math.max(map[i - 1][j], map[i][j]);
-                map[i][j] = Math.max(map[i - 1][j - 1], map[i][j]);
-                map[i][j] = Math.max(map[i][j - 1], map[i][j]);
-            }
-        }
+        bw.write(String.valueOf(answer));
 
-        for (int y = 0; y < n; y++) {
-            for (int x = 0; x < n; x++) {
-                bw.write(map[y][x] + " ");
-            }
-            bw.newLine();
-        }
 
         br.close();
         bw.close();
+    }
+
+    static int run(int[] arr, int cnt) {
+        for (int i = 1; i < n; i++) {
+            if (arr[i - 1] != b[i - 1]) {
+                change(arr, i - 1);
+                change(arr, i);
+                if (i != n - 1) change(arr, i + 1);
+                cnt++;
+            }
+        }
+        if (arr[n - 1] != b[n - 1]) cnt = -1;
+        return cnt;
+    }
+
+    static void change(int[] arr, int index) {
+        if (arr[index] == 0) arr[index] = 1;
+        else arr[index] = 0;
     }
 }
