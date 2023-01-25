cccac042355eba1446cb9a9b3d09ac7c5bf219f4
EunChanNam <namhm23@gmail.com>
2023-01-25 18:34:12 +0900

    백준 10159 저울

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -4,12 +4,26 @@ import java.util.*;
 
 public class Main {
     static int n;
+    static int m;
+    static List<List<Integer>> list1 = new ArrayList<>();
+    static List<List<Integer>> list2 = new ArrayList<>();
+    static int[] check;
 
-    static class Node{
-        int val; int k;
-        public Node(int val, int k) {
-            this.val = val;
-            this.k = k;
+    static void bfs(int start, List<List<Integer>> list) {
+        Queue<Integer> que = new LinkedList<>();
+        que.offer(start);
+        int[] visit = new int[n + 1];
+        visit[start] = 1;
+        int cnt = 0;
+        while (!que.isEmpty()) {
+            int now = que.poll();
+            check[now] = 1;
+            cnt++;
+            for (int next : list.get(now)) {
+                if (visit[next] == 1) continue;
+                visit[next] = 1;
+                que.offer(next);
+            }
         }
     }
     public static void main(String[] args) throws IOException {
@@ -18,42 +32,31 @@ public class Main {
         StringTokenizer st;
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
-        int len; int w;
-        len = Integer.parseInt(st.nextToken());
-        w = Integer.parseInt(st.nextToken());
-
-        Queue<Integer> que = new LinkedList<>();
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            que.offer(Integer.parseInt(st.nextToken()));
+        m = Integer.parseInt(br.readLine());
+        for (int i = 0; i <= n; i++) {
+            list1.add(new ArrayList<>());
+            list2.add(new ArrayList<>());
         }
 
-        List<Node> list = new ArrayList<>();
-        int size = 0;
-        int sum = 0;
-        int answer = 0;
-        while (!list.isEmpty() || !que.isEmpty()) {
-            answer++;
-            for (int i = list.size() - 1; i >= 0; i--) {
-                list.set(i, new Node(list.get(i).val, list.get(i).k - 1));
-                if (list.get(i).k == 0) {
-                    size--;
-                    sum -= list.get(i).val;
-                    list.remove(i);
-                }
-            }
-            if (!que.isEmpty()) {
-                int p = que.peek();
-                if (size + 1 <= len && sum + p <= w) {
-                    list.add(new Node(p, len));
-                    size += 1;
-                    sum += p;
-                    que.poll();
-                }
-            }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            list1.get(a).add(b);
+            list2.get(b).add(a);
         }
 
-        bw.write(String.valueOf(answer));
+        for (int i = 1; i <= n; i++) {
+            check = new int[n + 1];
+            bfs(i, list1);
+            bfs(i, list2);
+            int answer = n;
+            for (int j = 1; j <= n; j++) {
+                if (check[j] == 1) answer--;
+            }
+            bw.write(String.valueOf(answer));
+            bw.newLine();
+        }
 
         br.close();
         bw.close();
