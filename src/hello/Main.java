9473c4fba651e76a4eb14b527b80b90c93a299da
EunChanNam <namhm23@gmail.com>
2023-01-25 18:56:01 +0900

    프로그래머스 49191 순위

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -9,22 +9,47 @@ public class Main {
     static List<List<Integer>> list2 = new ArrayList<>();
     static int[] check;
 
-    static void bfs(int start, List<List<Integer>> list) {
+    static void bfs(List<List<Integer>> list, int start){
         Queue<Integer> que = new LinkedList<>();
         que.offer(start);
         int[] visit = new int[n + 1];
         visit[start] = 1;
-        int cnt = 0;
-        while (!que.isEmpty()) {
+        while(!que.isEmpty()){
             int now = que.poll();
             check[now] = 1;
-            cnt++;
-            for (int next : list.get(now)) {
+            for (int next : list.get(now)){
                 if (visit[next] == 1) continue;
                 visit[next] = 1;
                 que.offer(next);
             }
         }
+    }
+    public int solution(int n3, int[][] arr) {
+        int answer = 0;
+        n = n3;
+        m = arr.length;
+        for (int i=0; i <= n; i++){
+            list1.add(new ArrayList<>());
+            list2.add(new ArrayList<>());
+        }
+
+        for (int i=0; i < m; i++){
+            list1.get(arr[i][0]).add(arr[i][1]);
+            list2.get(arr[i][1]).add(arr[i][0]);
+        }
+
+        for (int i=1; i <= n; i++){
+            check = new int[n + 1];
+            bfs(list1, i);
+            bfs(list2, i);
+            int ret = n;
+            for (int j=1; j <= n; j++){
+                if (check[j] == 1) ret--;
+            }
+            if (ret == 0) answer++;
+        }
+
+        return answer;
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -32,31 +57,7 @@ public class Main {
         StringTokenizer st;
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(br.readLine());
-        for (int i = 0; i <= n; i++) {
-            list1.add(new ArrayList<>());
-            list2.add(new ArrayList<>());
-        }
 
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            list1.get(a).add(b);
-            list2.get(b).add(a);
-        }
-
-        for (int i = 1; i <= n; i++) {
-            check = new int[n + 1];
-            bfs(i, list1);
-            bfs(i, list2);
-            int answer = n;
-            for (int j = 1; j <= n; j++) {
-                if (check[j] == 1) answer--;
-            }
-            bw.write(String.valueOf(answer));
-            bw.newLine();
-        }
 
         br.close();
         bw.close();
