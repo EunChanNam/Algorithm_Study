42a036cddbfb7c9834bf3f1643077875b8df2ffe
EunChanNam <namhm23@gmail.com>
2023-01-27 13:58:43 +0900

    백준 20040 사이클 게임

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -5,51 +5,19 @@ import java.util.*;
 public class Main {
     static int n;
     static int m;
-    static List<List<Integer>> list1 = new ArrayList<>();
-    static List<List<Integer>> list2 = new ArrayList<>();
-    static int[] check;
+    static int[] arr;
 
-    static void bfs(List<List<Integer>> list, int start){
-        Queue<Integer> que = new LinkedList<>();
-        que.offer(start);
-        int[] visit = new int[n + 1];
-        visit[start] = 1;
-        while(!que.isEmpty()){
-            int now = que.poll();
-            check[now] = 1;
-            for (int next : list.get(now)){
-                if (visit[next] == 1) continue;
-                visit[next] = 1;
-                que.offer(next);
-            }
-        }
+    static int find(int a) {
+        if (a == arr[a]) return a;
+        else return arr[a] = find(arr[a]);
     }
-    public int solution(int n3, int[][] arr) {
-        int answer = 0;
-        n = n3;
-        m = arr.length;
-        for (int i=0; i <= n; i++){
-            list1.add(new ArrayList<>());
-            list2.add(new ArrayList<>());
-        }
 
-        for (int i=0; i < m; i++){
-            list1.get(arr[i][0]).add(arr[i][1]);
-            list2.get(arr[i][1]).add(arr[i][0]);
+    static void union(int a, int b) {
+        int fa = find(a);
+        int fb = find(b);
+        if (fa != fb) {
+            arr[fa] = fb;
         }
-
-        for (int i=1; i <= n; i++){
-            check = new int[n + 1];
-            bfs(list1, i);
-            bfs(list2, i);
-            int ret = n;
-            for (int j=1; j <= n; j++){
-                if (check[j] == 1) ret--;
-            }
-            if (ret == 0) answer++;
-        }
-
-        return answer;
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -57,7 +25,28 @@ public class Main {
         StringTokenizer st;
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = i;
+        }
 
+        int answer = 0;
+        int cnt = 0;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            cnt++;
+            if (find(a) == find(b)) {
+                answer = cnt;
+                break;
+            }
+            union(a, b);
+        }
+
+        bw.write(String.valueOf(answer));
 
         br.close();
         bw.close();
