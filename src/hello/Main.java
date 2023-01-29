44076fbed202ba401d5138b40cd7e17ca59c7585
EunChanNam <namhm23@gmail.com>
2023-01-29 15:18:16 +0900

    백준 2573 빙산

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -4,49 +4,61 @@ import java.util.*;
 
 public class Main {
     static int n;
+    static int m;
     static int[][] map;
-    static int totalMine = 0;
-    static int[][] dir = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+    static int[][] visit;
+    static int[][] visit2;
+    static int[][] dir = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
 
     static class Node{
-        int y; int x;
+        int y; int x; int cnt = 0;
         public Node(int y, int x){
             this.y = y;
             this.x = x;
         }
     }
-    static void findMine(int y, int x) {
-        int total = map[y][x]; // 목표 지뢰 개수
-        int mineCnt = 0; // 지뢰 개수
-        int shCnt = 0;  // 샵 개수
-        boolean isZero = false;
-        if (total == 0) isZero = true;
+    static void bfs(int y, int x) {
+        Queue<Node> que = new LinkedList<>();
+        que.offer(new Node(y, x));
+        List<Node> list = new ArrayList<>();
 
-        List<Node> list = new ArrayList<>(); // 샵 좌표
-        //-1 -> #, -2 -> 지뢰, -3 -> 공백
-        for (int i = 0; i < 8; i++) {
-            int ny = y + dir[i][0];
-            int nx = x + dir[i][1];
-            if (ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
-            if (map[ny][nx] >= 0) continue;
-            if (isZero) {
-                map[ny][nx] = -3;
-                continue;
+        while (!que.isEmpty()) {
+            Node p = que.poll();
+            for (int i = 0; i < 4; i++) {
+                int ny = p.y + dir[i][0];
+                int nx = p.x + dir[i][1];
+                if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+                if (visit[ny][nx] == 1) continue;
+                if (map[ny][nx] == 0) {
+                    p.cnt++;
+                    continue;
+                }
+                visit[ny][nx] = 1;
+                que.offer(new Node(ny, nx));
             }
-            if (map[ny][nx] == -1) {
-                shCnt++;
-                list.add(new Node(ny, nx));
-            }
-            if (map[ny][nx] == -2) mineCnt++;
+            list.add(p);
         }
-        if (mineCnt == total) {
-            for (Node p : list) {
-                map[p.y][p.x] = -3;
-            }
-        } else if (mineCnt + shCnt == total) {
-            for (Node p : list) {
-                map[p.y][p.x] = -2;
-                totalMine++;
+        for (Node p : list) {
+            int val = map[p.y][p.x] - p.cnt;
+            if (val < 0) val = 0;
+            map[p.y][p.x] = val;
+        }
+    }
+
+    static void check(int y, int x) {
+        Queue<Node> que = new LinkedList<>();
+        que.offer(new Node(y, x));
+
+        while (!que.isEmpty()) {
+            Node p = que.poll();
+            for (int i = 0; i < 4; i++) {
+                int ny = p.y + dir[i][0];
+                int nx = p.x + dir[i][1];
+                if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+                if (visit2[ny][nx] == 1) continue;
+                if (map[ny][nx] == 0) continue;
+                visit2[ny][nx] = 1;
+                que.offer(new Node(ny, nx));
             }
         }
     }
@@ -56,38 +68,49 @@ public class Main {
         StringTokenizer st;
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
-        map = new int[n][n];
-
+        m = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
         for (int y = 0; y < n; y++) {
-            String str = br.readLine();
-            for (int x = 0; x < str.length(); x++) {
-                char ch = str.charAt(x);
-                int t;
-                if (ch == '#') {
-                    t = -1;
-                } else t = ch - '0';
-                map[y][x] = t;
+            st = new StringTokenizer(br.readLine());
+            for (int x = 0; x < m; x++) {
+                map[y][x] = Integer.parseInt(st.nextToken());
             }
         }
 
-        for (int x = 0; x < n; x++) {
-            findMine(0, x);
-        }
-        for (int y = 1; y < n; y++) {
-            findMine(y, 0);
-        }
-        for (int x = 1; x < n; x++) {
-            findMine(n - 1, x);
-        }
-        for (int y = 1; y < n - 1; y++) {
-            findMine(y, n - 1);
+        int answer = 0;
+        while (true) {
+            int rand = 0;
+            answer++;
+            visit = new int[n][m];
+            for (int y = 0; y < n; y++) {
+                for (int x = 0; x < m; x++) {
+                    if (map[y][x] == 0) continue;
+                    if (visit[y][x] == 1) continue;
+                    visit[y][x] = 1;
+                    bfs(y, x);
+                }
+            }
+
+            visit2 = new int[n][m];
+            for (int y = 0; y < n; y++) {
+                for (int x = 0; x < m; x++) {
+                    if (map[y][x] == 0) continue;
+                    if (visit2[y][x] == 1) continue;
+                    visit2[y][x] = 1;
+                    check(y, x);
+                    rand++;
+                }
+            }
+            if (rand == 0){
+                answer = 0;
+                break;
+            }
+            if (rand > 1) {
+                break;
+            }
         }
 
-        if (n > 4) {
-            totalMine += (n - 4) * (n - 4);
-        }
-
-        bw.write(String.valueOf(totalMine));
+        bw.write(String.valueOf(answer));
 
         br.close();
         bw.close();
