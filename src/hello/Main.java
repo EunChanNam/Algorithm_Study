56e942af03548e9d8ab05aa61660fc3d1763cf47
EunChanNam <namhm23@gmail.com>
2023-02-28 15:40:13 +0900

    백준 1941 소문난 칠공주

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -4,63 +4,74 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n;
-    static int m;
-    static int[][] map;
-    static int[][] visit;
-    static int[][] dir = {{-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
-
+    static char[][] map = new char[5][5];
+    static List<Point> list = new ArrayList<>();
+    static int[][] dir = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
     static int answer = 0;
-    static void dfs(int y, int x, int sum) {
-        if (y >= n) return;
-        if (visit[y][x] == 1) {
-            if (x < m - 1) dfs(y, x + 1, sum);
-            else dfs(y + 1, 0, sum);
+    static class Point{
+        int y; int x;
+        public Point(int y, int x){
+            this.y = y;
+            this.x = x;
+        }
+    }
+
+    static Stack<Point> stack = new Stack<>();
+    static void run(int level, int start) {
+        if (level == 7) {
+            if (check()) answer++;
             return;
         }
-
-        //skip 하는 부분
-        if (x < m - 1) dfs(y, x + 1, sum);
-        else dfs(y + 1, 0, sum);
-        //연산 하는 부분
-        visit[y][x] = 1;
-        for (int i = 0; i < 4; i++) {
-            int ny = y + dir[i][0];
-            int nx = x + dir[i][1];
-            if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-            if (visit[ny][x] == 1 || visit[y][nx] == 1) continue;
-            visit[ny][x] = 1;
-            visit[y][nx] = 1;
-            int nextSum = sum;
-            nextSum += map[y][x] * 2;
-            nextSum += map[ny][x];
-            nextSum += map[y][nx];
-            answer = Math.max(answer, nextSum);
-            if (x < m - 1) dfs(y, x + 1, nextSum);
-            else dfs(y + 1, 0, nextSum);
-            visit[ny][x] = 0;
-            visit[y][nx] = 0;
+        for (int i = start; i < 25; i++) {
+            stack.push(list.get(i));
+            run(level + 1, i + 1);
+            stack.pop();
         }
-        visit[y][x] = 0;
+    }
+    static boolean check() {
+        int[][] temp = new int[5][5];
+        int sCnt = 0; int cnt = 0;
+        for (Point p : stack) temp[p.y][p.x] = 1;
+        Point s = stack.peek();
+        int[][] visit = new int[5][5];
+        visit[s.y][s.x] = 1;
+        Queue<Point> que = new LinkedList<>();
+        que.offer(s);
+
+        while (!que.isEmpty()) {
+            Point p = que.poll();
+            if (temp[p.y][p.x] == 1) cnt++;
+            if (map[p.y][p.x] == 'S') {
+                sCnt++;
+            }
+            for (int i = 0; i < 4; i++) {
+                int ny = p.y + dir[i][0];
+                int nx = p.x + dir[i][1];
+                if (ny < 0 || nx < 0 || ny >= 5 || nx >= 5) continue;
+                if (visit[ny][nx] == 1) continue;
+                visit[ny][nx] = 1;
+                if (temp[ny][nx] == 1) que.offer(new Point(ny, nx));
+            }
+        }
+        return cnt == 7 && sCnt >= 4;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        map = new int[n][m];
-        for (int y = 0; y < n; y++) {
-            st = new StringTokenizer(br.readLine());
-            for (int x = 0; x < m; x++) {
-                map[y][x] = Integer.parseInt(st.nextToken());
+//        st = new StringTokenizer(br.readLine());
+//        n = Integer.parseInt(st.nextToken());
+//        m = Integer.parseInt(st.nextToken());
+        for (int y = 0; y < 5; y++) {
+            String str = br.readLine();
+            for (int x = 0; x < 5; x++) {
+                map[y][x] = str.charAt(x);
+                list.add(new Point(y, x));
             }
         }
 
-        visit = new int[n][m];
-        dfs(0, 0, 0);
+        run(0, 0);
 
         bw.write(String.valueOf(answer));
 
