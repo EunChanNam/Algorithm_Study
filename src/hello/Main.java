5784429d80851e28e288d14082c6db9ebbe94964
EunChanNam <namhm23@gmail.com>
2023-03-02 15:03:10 +0900

    백준 1765 닭싸움 팀 정하기

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -5,44 +5,65 @@ import java.util.*;
 
 public class Main {
     static int n;
-    static int[][] map = new int[11][11];
-    static int[] visit;
+    static int m;
+    static int[] arr;
+    static List<List<Integer>> list = new ArrayList<>();
 
-    static int answer;
-    static void run(int level, int sum) {
-        if (level == 11) {
-            answer = Math.max(answer, sum);
-            return;
-        }
+    static int find(int a) {
+        if (arr[a] == a) return a;
+        else return arr[a] = find(arr[a]);
+    }
 
-        for (int i = 0; i < n; i++) {
-            if (visit[i] == 1) continue;
-            if (map[level][i] == 0) continue;
-            visit[i] = 1;
-            run(level + 1, sum + map[level][i]);
-            visit[i] = 0;
-        }
+    static void union(int a, int b) {
+        int fa = find(a);
+        int fb = find(b);
+        if (fa != fb) arr[fa] = fb;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        n = 11;
-        int k = Integer.parseInt(br.readLine());
-        for (int i=0; i < k; i++){
-            for (int y = 0; y < n; y++) {
-                st = new StringTokenizer(br.readLine());
-                for (int x = 0; x < n; x++) {
-                    map[y][x] = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(br.readLine());
+        arr = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i <= n; i++) {
+            arr[i] = i;
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            String s = st.nextToken();
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (s.equals("F")) {
+                union(a, b);
+            } else {
+                list.get(a).add(b);
+                list.get(b).add(a);
+            }
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            List<Integer> temp = list.get(i);
+            for (int k : temp) {
+                List<Integer> temp2 = list.get(k);
+                for (int t : temp2) {
+                    union(i, t);
                 }
             }
-            answer = Integer.MIN_VALUE;
-            visit = new int[11];
-            run(0, 0);
-            bw.write(String.valueOf(answer));
-            bw.newLine();
         }
+
+        Set<Integer> set = new HashSet<>();
+        for (int i = 1; i <= n; i++) {
+            set.add(find(i));
+        }
+
+        bw.write(String.valueOf(set.size()));
 
         br.close();
         bw.close();
