3357128fd12a32536a5f009c70d3426f920698a1
EunChanNam <namhm23@gmail.com>
2023-03-22 15:18:45 +0900

    백준 2170 선 긋기

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -4,69 +4,56 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n;
-    static int m;
-    static int[][] map;
-    static int[][] dir = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
 
-    static class Node{
-        int y; int x; int level; int b;
-        public Node(int y, int x, int level, int b) {
-            this.y = y;
-            this.x = x;
-            this.level = level;
-            this.b = b;
+    static class Node {
+        int start; int end;
+        public Node(int start, int end){
+            this.start = start;
+            this.end = end;
         }
-    }
-
-    static int bfs(Node start) {
-        Queue<Node> que = new LinkedList<>();
-        que.offer(start);
-        int[][][] visit = new int[2][n][m];
-        visit[0][0][0] = 1;
-        visit[1][0][0] = 1;
-
-        while (!que.isEmpty()) {
-            Node p = que.poll();
-            if (p.y == n - 1 && p.x == m - 1) return p.level;
-
-            for (int i = 0; i < 4; i++) {
-                int ny = p.y + dir[i][0];
-                int nx = p.x + dir[i][1];
-                if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-                if (visit[p.b][ny][nx] == 1) continue; // 단순 방문처리
-                if (p.b == 1 && visit[0][ny][nx] == 1) continue; // 그리디한 방문처리
-                if (p.b == 1 && map[ny][nx] == 1) continue; // 벽을 더이상 못부숨
-                if (p.b == 0 && map[ny][nx] == 1) { // 벽읇 부술 때
-                    visit[1][ny][nx] = 1;
-                    que.offer(new Node(ny, nx, p.level + 1, 1));
-                    continue;
-                }
-                visit[p.b][ny][nx] = 1;
-                que.offer(new Node(ny, nx, p.level + 1, p.b));
-            }
-        }
-        return -1;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        map = new int[n][m];
 
-        for (int y = 0; y < n; y++) {
-            String s = br.readLine();
-            for (int x = 0; x < m; x++) {
-                map[y][x] = s.charAt(x) - '0';
+        int n = Integer.parseInt(st.nextToken());
+
+        Node[] arr = new Node[n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            arr[i] = new Node(a, b);
+        }
+
+        Arrays.sort(arr, (a, b) -> {
+            if (a.start == b.start) {
+                return Integer.compare(a.end, b.end);
+            }
+            return Integer.compare(a.start, b.start);
+        });
+
+        int start = arr[0].start;
+        int end = arr[0].end;
+        int len = 0;
+        for (int i = 1; i < n; i++) {
+            Node now = arr[i];
+            if (now.start > end) {
+                len += end - start;
+                start = now.start;
+                end = now.end;
+                continue;
+            }
+            if (now.end > end) {
+                end = now.end;
             }
         }
 
-        int result = bfs(new Node(0, 0, 1, 0));
+        int answer = len + end - start;
 
-        bw.write(String.valueOf(result));
+        bw.write(String.valueOf(answer));
 
         br.close();
         bw.close();
