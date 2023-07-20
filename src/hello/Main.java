8715836d6c5171be019dbac329e5f38a5e845d84
EunChanNam <namhm23@gmail.com>
2023-07-20 15:32:58 +0900

    프로그래머스 레벨 2 광물 캐기

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -1,86 +1,75 @@
 package hello;
 
-import java.io.*;
-import java.util.*;
-import java.util.stream.IntStream;
-
 public class Main {
+    static int[] picks;
+    static String[] minerals;
 
-    static int n;
-    static int k;
-    static int combo;
-    static int coupon;
-
-    static List<Integer> dishes = new ArrayList<>();
-
-    static void initSlide(Map<Integer, Integer> slide) {
-        IntStream.range(0, combo)
-                .forEach(i -> {
-                    int target = dishes.get(i);
-                    slide.put(target, slide.getOrDefault(target, 0) + 1);
-                });
-    }
-
-    static void goNextSlide(int start, int end, Map<Integer, Integer> slide) {
-        int startDish = dishes.get(start);
-        int endDish = dishes.get(end);
-
-        if (slide.get(startDish) == 1) {
-            slide.remove(startDish);
-        } else slide.put(startDish, slide.get(startDish) - 1);
-
-        slide.put(endDish, slide.getOrDefault(endDish, 0) + 1);
-    }
-
-    static int findMaxDish() {
-
-        int start = 0;
-        int end =  combo - 1;
-
-        Map<Integer, Integer> slide = new HashMap<>();
-
-        initSlide(slide);
-        int result = slide.size();
-
-        do {
-            if (end == n - 1) end = -1;
-            goNextSlide(start, end + 1, slide);
-
-            int nowSize = slide.size();
-            if (!slide.containsKey(coupon)) {
-                nowSize++;
-            }
-
-            result = Math.max(result, nowSize);
-
-            start++;
-            end++;
-            if (start == n) start = 0;
-        } while (end != combo - 1);
-
-        return result;
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-        combo = Integer.parseInt(st.nextToken());
-        coupon = Integer.parseInt(st.nextToken());
-
-        for (int i = 0; i < n; i++) {
-            int input = Integer.parseInt(br.readLine());
-            dishes.add(input);
+    static int min = Integer.MAX_VALUE;
+    static void mine(int level, int pickIdx, int sum, int count) {
+        if (level == minerals.length) {
+            min = Math.min(sum, min);
+            return;
         }
 
-        int answer = findMaxDish();
+        int result = processMine(level, pickIdx);
 
-        bw.write(String.valueOf(answer));
+        if (count > 0) {
+            mine(level + 1, pickIdx, sum + result, count - 1);
+            return;
+        }
 
-        br.close();
-        bw.close();
+        if (notExistPick()) {
+            min = Math.min(sum, min);
+            return;
+        }
+
+
+        for (int i = 0; i < 3; i++) {
+            if (picks[i] <= 0) continue;
+            picks[i]--;
+            mine(level, i, sum, 5);
+            picks[i]++;
+        }
+    }
+
+    private static boolean notExistPick() {
+        for (int pick : picks) {
+            if (pick > 0) return false;
+        }
+        return true;
+    }
+
+    private static int processMine(int level, int pickIdx) {
+        if (pickIdx == 0) return 1;
+        if (pickIdx == 1) {
+            if (minerals[level].equals("diamond")) return 5;
+            else return 1;
+        }
+        if (pickIdx == 2) {
+            if (minerals[level].equals("diamond")) return 25;
+            if (minerals[level].equals("iron")) return 5;
+            else return 1;
+        }
+        throw new IllegalStateException();
+    }
+
+    static int solution(int[] p, String[] m) {
+        picks = p;
+        minerals = m;
+
+        for (int i = 0; i < 3; i++) {
+            if (picks[i] <= 0) continue;
+            picks[i]--;
+            mine(0, i, 0, 5);
+            picks[i]++;
+        }
+
+        return min;
+    }
+
+    public static void main(String[] args) {
+        int[] picks = {0, 1, 1};
+        String[] minerals = {"diamond", "diamond", "diamond", "diamond", "diamond", "iron", "iron", "iron", "iron", "iron", "diamond"};
+        System.out.println(solution(picks, minerals));
     }
 }
