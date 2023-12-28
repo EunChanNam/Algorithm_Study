37ff7b293f4d11422153782b69968c4eb9a9657c
EunChanNam <namhm23@gmail.com>
2023-12-28 16:30:24 +0900

    백준 14891 톱니바퀴 (재귀 풀이)

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -6,28 +6,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	static class SpinEvent {
-		int targetId;
-		int dir;
-
-		public SpinEvent(int targetId, int dir) {
-			this.targetId = targetId;
-			this.dir = dir;
-		}
-	}
-
 	static List<List<Integer>> top = new ArrayList<>();
 	static int n;
-	static Queue<SpinEvent> spinQueue = new LinkedList<>();
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -49,13 +36,7 @@ public class Main {
 			int targetId = Integer.parseInt(st.nextToken()) - 1;
 			int dir = Integer.parseInt(st.nextToken());
 
-			spinQueue.offer(new SpinEvent(targetId, dir));
-			run(targetId, dir);
-
-			while (!spinQueue.isEmpty()) {
-				SpinEvent event = spinQueue.poll();
-				spin(event.dir, top.get(event.targetId));
-			}
+			run(targetId, dir, 0);
 		}
 
 		int sum = 0;
@@ -81,40 +62,44 @@ public class Main {
 		bw.close();
 	}
 
-	static void run(int targetId, int dir) {
-		int leftDir = dir;
-		int rightDir = dir;
-		for (int i = targetId; i >= 1; i--) {
-			int leftId = i - 1;
-			leftDir = -leftDir;
-			List<Integer> now = top.get(i);
-			List<Integer> left = top.get(leftId);
-			if (!now.get(6).equals(left.get(2))) {
-				spinQueue.offer(new SpinEvent(leftId, leftDir));
-			} else break;
+	static void run(int targetId, int dir, int flow) {
+		List<Integer> target = top.get(targetId);
+		int right = target.get(2);
+		int left = target.get(6);
+
+		int leftId = targetId - 1;
+		if (leftId >= 0 && flow != 1) {
+			List<Integer> leftTarget = top.get(leftId);
+			int leftRight = leftTarget.get(2);
+			if (leftRight != left) {
+				run(leftId, -dir, -1);
+			}
 		}
 
-		for (int i = targetId; i < 3; i++) {
-			int rightId = i + 1;
-			rightDir = -rightDir;
-			List<Integer> now = top.get(i);
-			List<Integer> right = top.get(rightId);
-			if (!now.get(2).equals(right.get(6))) {
-				spinQueue.offer(new SpinEvent(rightId, rightDir));
-			} else break;
+		int rightId = targetId + 1;
+		if (rightId < 4 && flow != -1) {
+			List<Integer> rightTarget = top.get(rightId);
+			int rightLeft = rightTarget.get(6);
+			if (rightLeft != right) {
+				run(rightId, -dir, 1);
+			}
 		}
+
+		spin(dir, target);
 	}
 
 	static void spin(int dir, List<Integer> target) {
 		if (dir == 1) {
-			int last = target.get(7);
-			target.remove(target.size() - 1);
+			int lastIndex = target.size() - 1;
+			int last = target.get(lastIndex);
+			target.remove(lastIndex);
 			target.add(0, last);
 		}
 		else {
+			int lastIndex = target.size() - 1;
 			int first = target.get(0);
 			target.remove(0);
-			target.add(7, first);
+			target.add(lastIndex, first);
 		}
 	}
 }
