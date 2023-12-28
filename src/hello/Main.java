34ae32ae902363e23ca65380f5f41ffdfcf10e9c
EunChanNam <namhm23@gmail.com>
2023-12-28 16:24:28 +0900

    백준 14891 톱니바퀴

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -6,118 +6,115 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	static int[][] dir = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+	static class SpinEvent {
+		int targetId;
+		int dir;
+
+		public SpinEvent(int targetId, int dir) {
+			this.targetId = targetId;
+			this.dir = dir;
+		}
+	}
+
+	static List<List<Integer>> top = new ArrayList<>();
 	static int n;
-	static int m;
-	static int[][] map;
-	static int k;
-	static List<Integer> command = new ArrayList<>();
-
-	static void rowLeft(int[][] dice) {
-		int start = dice[1][0];
-		dice[1][0] = dice[3][1];
-		dice[3][1] = dice[1][2];
-		dice[1][2] = dice[1][1];
-		dice[1][1] = start;
-	}
-
-	static void rowRight(int[][] dice) {
-		int start = dice[1][2];
-		dice[1][2] = dice[3][1];
-		dice[3][1] = dice[1][0];
-		dice[1][0] = dice[1][1];
-		dice[1][1] = start;
-	}
-
-	static void colUp(int[][] dice) {
-		int start = dice[0][1];
-		dice[0][1] = dice[3][1];
-		dice[3][1] = dice[2][1];
-		dice[2][1] = dice[1][1];
-		dice[1][1] = start;
-	}
-
-	static void colDown(int[][] dice) {
-		int start = dice[2][1];
-		dice[2][1] = dice[3][1];
-		dice[3][1] = dice[0][1];
-		dice[0][1] = dice[1][1];
-		dice[1][1] = start;
-	}
+	static Queue<SpinEvent> spinQueue = new LinkedList<>();
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st;
 
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		int sy = Integer.parseInt(st.nextToken());
-		int sx = Integer.parseInt(st.nextToken());
-		k = Integer.parseInt(st.nextToken());
-		map = new int[n][m];
+		for (int i = 0; i < 4; i++) {
+			top.add(new ArrayList<>());
+			String s = br.readLine();
+			for (int j = 0; j < 8; j++) {
+				char ch = s.charAt(j);
+				top.get(i).add(ch - '0');
+			}
+		}
 
-		for (int y = 0; y < n; y++) {
+		n = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int x = 0; x < m; x++) {
-				map[y][x] = Integer.parseInt(st.nextToken());
+			int targetId = Integer.parseInt(st.nextToken()) - 1;
+			int dir = Integer.parseInt(st.nextToken());
+
+			spinQueue.offer(new SpinEvent(targetId, dir));
+			run(targetId, dir);
+
+			while (!spinQueue.isEmpty()) {
+				SpinEvent event = spinQueue.poll();
+				spin(event.dir, top.get(event.targetId));
 			}
 		}
 
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < k; i++) {
-			command.add(Integer.parseInt(st.nextToken()) - 1);
+		int sum = 0;
+		for (int i = 0; i < 4; i++) {
+			List<Integer> target = top.get(i);
+			if (i == 0 && target.get(0) == 1) {
+				sum += 1;
+			}
+			if (i == 1 && target.get(0) == 1) {
+				sum += 2;
+			}
+			if (i == 2 && target.get(0) == 1) {
+				sum += 4;
+			}
+			if (i == 3 && target.get(0) == 1) {
+				sum += 8;
+			}
 		}
 
-		int[][] dice = new int[4][3];
-
-		int ny = sy;
-		int nx = sx;
-		for (int i = 0; i < k; i++) {
-			int cmd = command.get(i);
-
-			ny += dir[cmd][0];
-			nx += dir[cmd][1];
-			if (ny < 0 || nx < 0 || ny >= n || nx >= m){
-				ny -= dir[cmd][0];
-				nx -= dir[cmd][1];
-				continue;
-			}
-
-			if (cmd == 0) {
-				rowRight(dice);
-				checkMapAndDice(dice, ny, nx);
-			} else if (cmd == 1) {
-				rowLeft(dice);
-				checkMapAndDice(dice, ny, nx);
-			} else if (cmd == 2) {
-				colUp(dice);
-				checkMapAndDice(dice, ny, nx);
-			} else if (cmd == 3) {
-				colDown(dice);
-				checkMapAndDice(dice, ny, nx);
-			}
-
-			bw.write(String.valueOf(dice[3][1]));
-			bw.newLine();
-		}
+		bw.write(String.valueOf(sum));
 
 		br.close();
 		bw.close();
 	}
 
-	private static void checkMapAndDice(int[][] dice, int ny, int nx) {
-		if (map[ny][nx] != 0) {
-			dice[1][1] = map[ny][nx];
-			map[ny][nx] = 0;
-		} else {
-			map[ny][nx] = dice[1][1];
+	static void run(int targetId, int dir) {
+		int leftDir = dir;
+		int rightDir = dir;
+		for (int i = targetId; i >= 1; i--) {
+			int leftId = i - 1;
+			leftDir = -leftDir;
+			List<Integer> now = top.get(i);
+			List<Integer> left = top.get(leftId);
+			if (!now.get(6).equals(left.get(2))) {
+				spinQueue.offer(new SpinEvent(leftId, leftDir));
+			} else break;
+		}
+
+		for (int i = targetId; i < 3; i++) {
+			int rightId = i + 1;
+			rightDir = -rightDir;
+			List<Integer> now = top.get(i);
+			List<Integer> right = top.get(rightId);
+			if (!now.get(2).equals(right.get(6))) {
+				spinQueue.offer(new SpinEvent(rightId, rightDir));
+			} else break;
+		}
+	}
+
+	static void spin(int dir, List<Integer> target) {
+		if (dir == 1) {
+			int last = target.get(7);
+			target.remove(target.size() - 1);
+			target.add(0, last);
+		}
+		else {
+			int first = target.get(0);
+			target.remove(0);
+			target.add(7, first);
 		}
 	}
 }
