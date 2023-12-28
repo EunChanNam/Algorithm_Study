9de907500c8bfdf2aae2045b6c75d48db2741b0d
EunChanNam <namhm23@gmail.com>
2023-12-28 14:27:49 +0900

    백준 14499 주사위 굴리기

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -5,91 +5,51 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int[][] dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-
-	static int getNextDir(int nowDir) {
-		if (nowDir == 0) return 3;
-		return nowDir - 1;
-	}
-
-	static int getBackDir(int nowDir) {
-		return getNextDir(getNextDir(nowDir));
-	}
-	/**
-	 * 1. 상하좌우에서 빈칸이 있는지 확인
-	 * 2. 없다면 현재 방향 반대 방향으로 후진 -> 후진 불가능하면 게임 종료
-	 * 3. 있다면 방향을 반시계 방향으로 회전
-	 * 4. 회전한 방향에 빈칸이 있다면 이동 -> 회전 방향에 빈칸이 없다면 다시 회전
-	 * 5. 청소를 진행하고 다시 1번으로 이동
-	 */
-
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+	static int[][] dir = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
 	static int n;
 	static int m;
 	static int[][] map;
+	static int k;
+	static List<Integer> command = new ArrayList<>();
 
-	static int solution(int startY, int startX, int startDir) {
-		int nowY = startY;
-		int nowX = startX;
-		int nowDir = startDir;
-		int count = 0;
+	static void rowLeft(int[][] dice) {
+		int start = dice[1][0];
+		dice[1][0] = dice[3][1];
+		dice[3][1] = dice[1][2];
+		dice[1][2] = dice[1][1];
+		dice[1][1] = start;
+	}
 
-		while (true) {
-			//청소
-			if (map[nowY][nowX] == 0) {
-				count++;
-				map[nowY][nowX] = -1;
-			}
+	static void rowRight(int[][] dice) {
+		int start = dice[1][2];
+		dice[1][2] = dice[3][1];
+		dice[3][1] = dice[1][0];
+		dice[1][0] = dice[1][1];
+		dice[1][1] = start;
+	}
 
-			//1
-			boolean flag = false;
-			for (int i = 0; i < 4; i++) {
-				int ny = nowY + dir[i][0];
-				int nx = nowX + dir[i][1];
-				if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-				if (map[ny][nx] == 0) {
-					flag = true;
-					break;
-				}
-			}
+	static void colUp(int[][] dice) {
+		int start = dice[0][1];
+		dice[0][1] = dice[3][1];
+		dice[3][1] = dice[2][1];
+		dice[2][1] = dice[1][1];
+		dice[1][1] = start;
+	}
 
-			//2
-			if (!flag) {
-				int backDir = getBackDir(nowDir);
-				int ny = nowY + dir[backDir][0];
-				int nx = nowX + dir[backDir][1];
-				if (ny < 0 || nx < 0 || ny >= n || nx >= m) {
-					break;
-				}
-				if (map[ny][nx] == 1) {
-					break;
-				}
-				nowY = ny;
-				nowX = nx;
-				continue;
-			}
-
-			//3
-			nowDir = getNextDir(nowDir);
-			int ny = nowY + dir[nowDir][0];
-			int nx = nowX + dir[nowDir][1];
-			if (ny < 0 || nx < 0 || ny >= n || nx >= m) {
-				continue;
-			}
-
-			//4
-			if (map[ny][nx] == 0) {
-				nowY = ny;
-				nowX = nx;
-			}
-		}
-
-		return count;
+	static void colDown(int[][] dice) {
+		int start = dice[2][1];
+		dice[2][1] = dice[3][1];
+		dice[3][1] = dice[0][1];
+		dice[0][1] = dice[1][1];
+		dice[1][1] = start;
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -98,13 +58,11 @@ public class Main {
 
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
-
-		st = new StringTokenizer(br.readLine());
-		int startY = Integer.parseInt(st.nextToken());
-		int startX = Integer.parseInt(st.nextToken());
-		int startDir = Integer.parseInt(st.nextToken());
-
+		int sy = Integer.parseInt(st.nextToken());
+		int sx = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
 		map = new int[n][m];
+
 		for (int y = 0; y < n; y++) {
 			st = new StringTokenizer(br.readLine());
 			for (int x = 0; x < m; x++) {
@@ -112,10 +70,54 @@ public class Main {
 			}
 		}
 
-		int answer = solution(startY, startX, startDir);
-		bw.write(String.valueOf(answer));
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < k; i++) {
+			command.add(Integer.parseInt(st.nextToken()) - 1);
+		}
+
+		int[][] dice = new int[4][3];
+
+		int ny = sy;
+		int nx = sx;
+		for (int i = 0; i < k; i++) {
+			int cmd = command.get(i);
+
+			ny += dir[cmd][0];
+			nx += dir[cmd][1];
+			if (ny < 0 || nx < 0 || ny >= n || nx >= m){
+				ny -= dir[cmd][0];
+				nx -= dir[cmd][1];
+				continue;
+			}
+
+			if (cmd == 0) {
+				rowRight(dice);
+				checkMapAndDice(dice, ny, nx);
+			} else if (cmd == 1) {
+				rowLeft(dice);
+				checkMapAndDice(dice, ny, nx);
+			} else if (cmd == 2) {
+				colUp(dice);
+				checkMapAndDice(dice, ny, nx);
+			} else if (cmd == 3) {
+				colDown(dice);
+				checkMapAndDice(dice, ny, nx);
+			}
+
+			bw.write(String.valueOf(dice[3][1]));
+			bw.newLine();
+		}
 
 		br.close();
 		bw.close();
+	}
+
+	private static void checkMapAndDice(int[][] dice, int ny, int nx) {
+		if (map[ny][nx] != 0) {
+			dice[1][1] = map[ny][nx];
+			map[ny][nx] = 0;
+		} else {
+			map[ny][nx] = dice[1][1];
+		}
 	}
 }
