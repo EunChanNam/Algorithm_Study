f052dbaa84fa568f6ccb9baa0bf1a2c313a37181
EunChanNam <namhm23@gmail.com>
2024-01-18 16:23:28 +0900

    백준 14891 17471 게더맨더링

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -6,100 +6,115 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	static List<List<Integer>> top = new ArrayList<>();
 	static int n;
+	static int[] people;
+	static List<List<Integer>> net = new ArrayList<>();
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
-		for (int i = 0; i < 4; i++) {
-			top.add(new ArrayList<>());
-			String s = br.readLine();
-			for (int j = 0; j < 8; j++) {
-				char ch = s.charAt(j);
-				top.get(i).add(ch - '0');
-			}
-		}
-
 		n = Integer.parseInt(br.readLine());
+		people = new int[n + 1];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= n; i++) {
+			people[i] = Integer.parseInt(st.nextToken());
+		}
 
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i <= n; i++) {
+			net.add(new ArrayList<>());
+		}
+
+		for (int i = 1; i <= n; i++) {
 			st = new StringTokenizer(br.readLine());
-			int targetId = Integer.parseInt(st.nextToken()) - 1;
-			int dir = Integer.parseInt(st.nextToken());
-
-			run(targetId, dir, 0);
-		}
-
-		int sum = 0;
-		for (int i = 0; i < 4; i++) {
-			List<Integer> target = top.get(i);
-			if (i == 0 && target.get(0) == 1) {
-				sum += 1;
-			}
-			if (i == 1 && target.get(0) == 1) {
-				sum += 2;
-			}
-			if (i == 2 && target.get(0) == 1) {
-				sum += 4;
-			}
-			if (i == 3 && target.get(0) == 1) {
-				sum += 8;
+			int count = Integer.parseInt(st.nextToken());
+			for (int j = 0; j < count; j++) {
+				net.get(i).add(Integer.parseInt(st.nextToken()));
 			}
 		}
 
-		bw.write(String.valueOf(sum));
+		dfs(1);
+
+		if (answer == Integer.MAX_VALUE) {
+			bw.write("-1");
+		} else {
+			bw.write(String.valueOf(answer));
+		}
 
 		br.close();
 		bw.close();
 	}
 
-	static void run(int targetId, int dir, int flow) {
-		List<Integer> target = top.get(targetId);
-		int right = target.get(2);
-		int left = target.get(6);
-
-		int leftId = targetId - 1;
-		if (leftId >= 0 && flow != 1) {
-			List<Integer> leftTarget = top.get(leftId);
-			int leftRight = leftTarget.get(2);
-			if (leftRight != left) {
-				run(leftId, -dir, -1);
+	static Stack<Integer> area1 = new Stack<>();
+	static Stack<Integer> area2 = new Stack<>();
+	static int answer = Integer.MAX_VALUE;
+	private static void dfs(int level) {
+		if (level == n + 1) {
+			if (area1.isEmpty() || area2.isEmpty()) {
+				return;
 			}
+
+			int result1 = bfs(area1);
+			int result2 = bfs(area2);
+
+			if (result1 == -1 || result2 == -1) {
+				return;
+			}
+
+			answer = Math.min(answer, Math.abs(result1 - result2));
+			return;
 		}
 
-		int rightId = targetId + 1;
-		if (rightId < 4 && flow != -1) {
-			List<Integer> rightTarget = top.get(rightId);
-			int rightLeft = rightTarget.get(6);
-			if (rightLeft != right) {
-				run(rightId, -dir, 1);
-			}
-		}
+		area1.push(level);
+		dfs(level + 1);
+		area1.pop();
 
-		spin(dir, target);
+		area2.push(level);
+		dfs(level + 1);
+		area2.pop();
 	}
 
-	static void spin(int dir, List<Integer> target) {
-		if (dir == 1) {
-			int lastIndex = target.size() - 1;
-			int last = target.get(lastIndex);
-			target.remove(lastIndex);
-			target.add(0, last);
+	private static int bfs(Stack<Integer> area) {
+		Set<Integer> areaSet = new HashSet<>(area);
+		int totalPeople = 0;
+
+		int start = area.peek();
+		boolean[] visit = new boolean[n + 1];
+		visit[start] = true;
+		Queue<Integer> que = new LinkedList<>();
+		que.offer(start);
+
+		while (!que.isEmpty()) {
+			int now = que.poll();
+			totalPeople += people[now];
+
+			List<Integer> nowNet = net.get(now);
+			for (int next : nowNet) {
+				if (visit[next]) continue;
+				if (!areaSet.contains(next)) continue;
+				visit[next]	= true;
+				que.add(next);
+			}
 		}
-		else {
-			int lastIndex = target.size() - 1;
-			int first = target.get(0);
-			target.remove(0);
-			target.add(lastIndex, first);
+
+		for (int target : area) {
+			if (!visit[target]) {
+				return -1;
+			}
 		}
+
+		return totalPeople;
 	}
 }
