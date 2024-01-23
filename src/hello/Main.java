760cbf5740fe30e0d17a010d829bb286f84ed15a
EunChanNam <namhm23@gmail.com>
2024-01-23 18:32:15 +0900

    백준 15661 링크와 스타트

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -7,28 +7,14 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	static class Point{
-		int y;
-		int x;
-		public Point(int y, int x) {
-			this.y = y;
-			this.x = x;
-		}
-	}
-
 	static int n;
-	static int m;
-	static int h;
-	static List<Point> mints = new ArrayList<>();
-	static Point start;
-
+	static int[][] map;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -36,64 +22,58 @@ public class Main {
 
 		st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		h = Integer.parseInt(st.nextToken());
+		map = new int[n][n];
 
 		for (int y = 0; y < n; y++) {
 			st = new StringTokenizer(br.readLine());
 			for (int x = 0; x < n; x++) {
-				int value = Integer.parseInt(st.nextToken());
-				if (value == 2) mints.add(new Point(y, x));
-				if (value == 1) start = new Point(y, x);
+				map[y][x] = Integer.parseInt(st.nextToken());
 			}
 		}
 
-		visit = new boolean[mints.size()];
+		for (int i = 0; i < n; i++) {
+			team2.add(i);
+		}
+		dfs(0, 0);
 
-		dfs(0);
-
-		System.out.println(answer);
+		bw.write(String.valueOf(answer));
 
 		br.close();
 		bw.close();
 	}
 
-	static Stack<Point> stack = new Stack<>();
-	static boolean[] visit;
-	static int answer = 0;
-	static void dfs(int level) {
-		if (level == mints.size()) {
-			findMaxMint();
+	static List<Integer> team1 = new ArrayList<>();
+	static List<Integer> team2 = new ArrayList<>();
+	static int answer = Integer.MAX_VALUE;
+
+	static void dfs(int level, int start) {
+		if (!team1.isEmpty() && !team2.isEmpty()) {
+			int team1Score = getTeamScore(team1);
+			int team2Score = getTeamScore(team2);
+			answer = Math.min(answer, Math.abs(team1Score - team2Score));
 		}
 
-		for (int i = 0; i < mints.size(); i++) {
-			if (visit[i]) continue;
-			visit[i] = true;
-			stack.push(mints.get(i));
-			dfs(level + 1);
-			stack.pop();
-			visit[i] = false;
+		for (int i = start; i < n; i++) {
+			team1.add(i);
+			team2.remove(Integer.valueOf(i));
+			dfs(level + 1, i + 1);
+			team2.add(i);
+			team1.remove(team1.size() - 1);
 		}
 	}
 
-	static void findMaxMint() {
-		int nowHp = m;
-		Point now = new Point(start.y, start.x);
-		int cnt = 0;
-		for (Point mint : stack) {
-			int dis = Math.abs(mint.y - now.y) + Math.abs(mint.x - now.x);
-			if (dis > nowHp){
-				return;
-			}
-			cnt++;
-			nowHp -= dis;
-			nowHp += h;
-			now = new Point(mint.y, mint.x);
-
-			int homeDis = Math.abs(start.y - now.y) + Math.abs(start.x - now.x);
-			if (nowHp >= homeDis) {
-				answer = Math.max(answer, cnt);
+	static int getTeamScore(List<Integer> team) {
+		int m = team.size();
+		int result = 0;
+		for (int y = 0; y < m; y++) {
+			int a = team.get(y);
+			for (int x = 0; x < m; x++) {
+				if (y == x) continue;
+				int b = team.get(x);
+				result += map[a][b];
 			}
 		}
+
+		return result;
 	}
 }
