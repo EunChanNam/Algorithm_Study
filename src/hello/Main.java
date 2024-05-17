0e77e129294b19b46b2787344061bbfe16086ba0
EunChanNam <namhm23@gmail.com>
2024-05-17 13:54:11 +0900

    백준 1863 스카이라인 쉬운거

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -5,68 +5,66 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
+
+    private static class Building {
+
+        int start;
+        int height;
+
+        public Building(int start, int height) {
+            this.start = start;
+            this.height = height;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        arr = new int[n + 1];
-
-        for (int i = 1; i <= n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-        visit = new boolean[n + 1];
-        cycle = new boolean[n + 1];
-
-        for (int i = 1; i <= n; i++) {
-            if (cycle[i]) {
-                continue;
-            }
-            stack.push(i);
-            dfs(i, i);
-            stack.pop();
+        List<Building> buildings = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            buildings.add(new Building(a, b));
         }
 
         int cnt = 0;
-        for (int i = 1; i <= n; i++) {
-            if (cycle[i]) {
+        Stack<Integer> stack = new Stack<>();
+        for (Building building : buildings) {
+            while (!stack.isEmpty()) {
+                int top = stack.peek();
+                if (top > building.height) {
+                    stack.pop();
+                } else {
+                    break;
+                }
+            }
+
+            boolean flag = false;
+            for (int height : stack) {
+                if (height == building.height) {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (!flag && building.height != 0) {
                 cnt++;
+                stack.push(building.height);
             }
         }
 
         bw.write(String.valueOf(cnt));
-        for (int i = 1; i <= n; i++) {
-            if (cycle[i]) {
-                bw.newLine();
-                bw.write(String.valueOf(i));
-            }
-        }
 
         br.close();
         bw.close();
-    }
-
-    private static int[] arr;
-    private static boolean[] visit;
-    private static boolean[] cycle;
-    private static Stack<Integer> stack = new Stack<>();
-
-    private static void dfs(int start, int target) {
-        if (arr[start] == target) {
-            stack.forEach(idx -> cycle[idx] = true);
-            return;
-        }
-
-        if (!visit[arr[start]] && !cycle[arr[start]]) {
-            visit[arr[start]] = true;
-            stack.push(arr[start]);
-            dfs(arr[start], target);
-            stack.pop();
-            visit[arr[start]] = false;
-        }
     }
 }
