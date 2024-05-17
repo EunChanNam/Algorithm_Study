18f8403f7ece7c0f9b3163a17b2e07cc3400640a
EunChanNam <namhm23@gmail.com>
2024-05-17 13:00:47 +0900

    백준 2668 숫자 고르기

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -5,74 +5,68 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.Stack;
 
 public class Main {
-
-    private static class Node {
-
-        int target;
-        int cost;
-
-        public Node(int target, int cost) {
-            this.target = target;
-            this.cost = cost;
-        }
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        arr = new int[n + 1];
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        visit = new boolean[n + 1];
+        cycle = new boolean[n + 1];
 
-        List<List<Node>> map = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            map.add(new ArrayList<>());
+        for (int i = 1; i <= n; i++) {
+            if (cycle[i]) {
+                continue;
+            }
+            stack.push(i);
+            dfs(i, i);
+            stack.pop();
         }
 
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-
-            map.get(a).add(new Node(b, cost));
-            map.get(b).add(new Node(a, cost));
-        }
-
-        int[] dis = new int[n + 1];
-        for (int i = 0; i <= n; i++) {
-            dis[i] = Integer.MAX_VALUE;
-        }
-        dis[1] = 0;
-
-        PriorityQueue<Node> que = new PriorityQueue<>(Comparator.comparingInt(a -> a.cost));
-        que.offer(new Node(1, 0));
-
-        while (!que.isEmpty()) {
-            Node now = que.poll();
-
-            List<Node> nowMap = map.get(now.target);
-            for (Node node : nowMap) {
-                int nextCost = now.cost + node.cost;
-                if (nextCost < dis[node.target]) {
-                    dis[node.target] = nextCost;
-                    que.offer(new Node(node.target, nextCost));
-                }
+        int cnt = 0;
+        for (int i = 1; i <= n; i++) {
+            if (cycle[i]) {
+                cnt++;
             }
         }
 
-        bw.write(String.valueOf(dis[n]));
+        bw.write(String.valueOf(cnt));
+        for (int i = 1; i <= n; i++) {
+            if (cycle[i]) {
+                bw.newLine();
+                bw.write(String.valueOf(i));
+            }
+        }
 
         br.close();
         bw.close();
+    }
+
+    private static int[] arr;
+    private static boolean[] visit;
+    private static boolean[] cycle;
+    private static Stack<Integer> stack = new Stack<>();
+
+    private static void dfs(int start, int target) {
+        if (arr[start] == target) {
+            stack.forEach(idx -> cycle[idx] = true);
+            return;
+        }
+
+        if (!visit[arr[start]] && !cycle[arr[start]]) {
+            visit[arr[start]] = true;
+            stack.push(arr[start]);
+            dfs(arr[start], target);
+            stack.pop();
+            visit[arr[start]] = false;
+        }
     }
 }
