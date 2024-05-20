666a31f6fa8e6c1f32c014ab273635cb5b10b555
EunChanNam <namhm23@gmail.com>
2024-05-20 22:21:44 +0900

    프로그래머스 베스트앨범

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -1,105 +1,72 @@
-package hello;
+import java.util.*;
+import java.util.stream.*;
+class Solution {
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.StringTokenizer;
-
-public class Main {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int count = 0;
-        while (true) {
-            count++;
-            int n = Integer.parseInt(br.readLine());
-            if (n == 0) {
-                break;
-            }
-            int[][] map = new int[n][n];
-            for (int y = 0; y < n; y++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                for (int x = 0; x < n; x++) {
-                    map[y][x] = Integer.parseInt(st.nextToken());
-                }
-            }
-            solution(n, map, count, bw);
-        }
-
-        br.close();
-        bw.close();
-    }
-
-    private static class Node {
-        int y;
-        int x;
-        int cost;
-
-        public Node(int y, int x, int cost) {
-            this.y = y;
-            this.x = x;
-            this.cost = cost;
+    private class Node {
+        String genre;
+        int play;
+        int idx;
+        public Node(String genre, int play, int idx) {
+            this.genre = genre;
+            this.play = play;
+            this.idx = idx;
         }
     }
 
-    private static int[][] dir = {{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
+    public int[] solution(String[] genres, int[] plays) {
+        int n = genres.length;
+        List<Integer> answerList = new ArrayList<>();
 
-    private static void solution(int n, int[][] map, int count, BufferedWriter bw) throws IOException {
-        int[][] visit = new int[n][n];
-        for (int y = 0; y < n; y++) {
-            for (int x = 0; x < n; x++) {
-                visit[y][x] = Integer.MAX_VALUE;
+        Map<String, List<Node>> map = new HashMap<>();
+
+        for (int i=0; i < n; i++){
+            String genre = genres[i];
+            int play = plays[i];
+
+            if (map.get(genre) == null) {
+                map.put(genre, new ArrayList<>());
             }
+            map.get(genre).add(new Node(genre, play, i));
         }
-        visit[0][0] = map[0][0];
 
-        PriorityQueue<Node> que = new PriorityQueue<>((a, b) -> {
-            if (a.cost == b.cost) {
-                if (a.y == b.y) {
-                    return Integer.compare(b.x, a.x);
-                }
-                return Integer.compare(b.y, a.y);
+        List<List<Node>> list = map.values().stream().collect(Collectors.toList());
+        list.sort((a, b) -> {
+            int sumA = 0;
+            for (Node node : a) {
+                sumA += node.play;
             }
-            return Integer.compare(a.cost, b.cost);
+            int sumB = 0;
+            for (Node node : b) {
+                sumB += node.play;
+            }
+            return Integer.compare(sumB, sumA);
         });
-        que.offer(new Node(0, 0, map[0][0]));
 
-        while (!que.isEmpty()) {
-            Node now = que.poll();
+        list.forEach(nodes -> {
+            nodes.sort((a, b) -> {
+                if (a.play == b.play) {
+                    return Integer.compare(a.idx, b.idx);
+                }
+                return Integer.compare(b.play, a.play);
+            });
+        });
 
-            boolean finFlag = false;
-            for (int i = 0; i < 4; i++) {
-                int ny = now.y + dir[i][0];
-                int nx = now.x + dir[i][1];
-
-                if (ny >= n || nx >= n || ny < 0 || nx < 0) {
+        for (List<Node> nodes : list) {
+            int songCnt = 0;
+            for (Node node : nodes) {
+                if (songCnt == 2) {
                     continue;
                 }
-                int nextCost = now.cost + map[ny][nx];
-                if (nextCost < visit[ny][nx]) {
-                    visit[ny][nx] = nextCost;
-                    que.offer(new Node(ny, nx, nextCost));
-                    if (ny == n - 1 && nx == n - 1) {
-                        finFlag = true;
-                        break;
-                    }
-                }
-            }
-
-            if (finFlag) {
-                break;
+                answerList.add(node.idx);
+                songCnt++;
             }
         }
 
-        bw.write("Problem " + count + ": " + visit[n - 1][n - 1]);
-        bw.newLine();
+        int[] answer = new int[answerList.size()];
+        for (int i=0; i < answerList.size(); i++){
+            answer[i] = answerList.get(i);
+        }
+
+        return answer;
     }
 }
