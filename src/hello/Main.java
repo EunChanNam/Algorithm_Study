3ee094b5342c1f0fe65b9c232a7fde3e304e5b13
EunChanNam <namhm23@gmail.com>
2024-05-21 13:29:12 +0900

    백준 2179 비슷한 단어

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -1,72 +1,87 @@
-import java.util.*;
-import java.util.stream.*;
-class Solution {
+package hello;
 
-    private class Node {
-        String genre;
-        int play;
-        int idx;
-        public Node(String genre, int play, int idx) {
-            this.genre = genre;
-            this.play = play;
-            this.idx = idx;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Map<String, Integer> map = new HashMap<>();
+        Set<String> set = new HashSet<>();
+
+        int n = Integer.parseInt(br.readLine());
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            list.add(str);
         }
-    }
 
-    public int[] solution(String[] genres, int[] plays) {
-        int n = genres.length;
-        List<Integer> answerList = new ArrayList<>();
-
-        Map<String, List<Node>> map = new HashMap<>();
-
-        for (int i=0; i < n; i++){
-            String genre = genres[i];
-            int play = plays[i];
-
-            if (map.get(genre) == null) {
-                map.put(genre, new ArrayList<>());
+        list.forEach(str -> {
+            if (!set.contains(str)) {
+                StringBuilder sb = new StringBuilder();
+                for (char ch : str.toCharArray()) {
+                    sb.append(ch);
+                    String now = sb.toString();
+                    map.put(now, map.getOrDefault(now, 0) + 1);
+                }
+                set.add(str);
             }
-            map.get(genre).add(new Node(genre, play, i));
-        }
-
-        List<List<Node>> list = map.values().stream().collect(Collectors.toList());
-        list.sort((a, b) -> {
-            int sumA = 0;
-            for (Node node : a) {
-                sumA += node.play;
-            }
-            int sumB = 0;
-            for (Node node : b) {
-                sumB += node.play;
-            }
-            return Integer.compare(sumB, sumA);
         });
 
-        list.forEach(nodes -> {
-            nodes.sort((a, b) -> {
-                if (a.play == b.play) {
-                    return Integer.compare(a.idx, b.idx);
-                }
-                return Integer.compare(b.play, a.play);
-            });
-        });
+        List<String> keys = new ArrayList<>(map.keySet())
+            .stream()
+            .filter(key -> map.get(key) >= 2)
+            .collect(Collectors.toList());
+        keys.sort((a, b) -> Integer.compare(b.length(), a.length()));
+        int maxLength = keys.get(0).length();
 
-        for (List<Node> nodes : list) {
-            int songCnt = 0;
-            for (Node node : nodes) {
-                if (songCnt == 2) {
-                    continue;
+        List<String> maxKeys = new ArrayList<>();
+        for (String key : keys) {
+            if (key.length() != maxLength) {
+                break;
+            }
+            maxKeys.add(key);
+        }
+
+        int cnt = 0;
+        String maxKey = "";
+        for (String str : list) {
+            for (String key : maxKeys) {
+                if (str.startsWith(key)) {
+                    maxKey = key;
+                    break;
                 }
-                answerList.add(node.idx);
-                songCnt++;
+            }
+            if (!maxKey.isEmpty()) {
+                break;
+            }
+        }
+        for (String str : list) {
+            if (cnt == 2) {
+                break;
+            }
+
+            if (str.startsWith(maxKey)) {
+                System.out.println(str);
+                cnt++;
             }
         }
 
-        int[] answer = new int[answerList.size()];
-        for (int i=0; i < answerList.size(); i++){
-            answer[i] = answerList.get(i);
-        }
-
-        return answer;
+        br.close();
+        bw.close();
     }
 }
