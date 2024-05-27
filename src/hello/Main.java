2c149579a2472f8a35ee23b52e8a2b353e31085c
EunChanNam <namhm23@gmail.com>
2024-05-27 13:21:06 +0900

    LeetCode 899 Orderly Queue

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -1,36 +1,33 @@
 import java.util.*;
 class Solution {
-    public long putMarbles(int[] weights, int k) {
-        int n = weights.length;
+    public String orderlyQueue(String s, int k) {
+        if (k == 1) {
+            List<String> resultList = findByBfs(s, k);
+            resultList.sort((a, b) -> a.compareTo(b));
+            return resultList.get(0);
+        } else {
+            char[] charArray = s.toCharArray();
+            Arrays.sort(charArray);
+            return String.valueOf(charArray);
+        }
+    }
 
+    private List<String> findByBfs(String s, int k) {
+        int n = s.length();
 
-        long leftStart = weights[0];
-        long rightEnd = weights[n - 1];
+        List<String> result = new ArrayList<>();
+        String now = s;
+        for (int i=0; i < n; i++) {
+            StringBuilder sb = new StringBuilder(now);
 
-        List<Long> sumList = new ArrayList<>();
-        for (int i=0; i < n - 1; i++) {
-            long nowWeight = weights[i];
-            long nextWeight = weights[i + 1];
-            long sum = nowWeight + nextWeight;
+            char first = now.charAt(0);
+            sb.deleteCharAt(0);
+            sb.append(first);
 
-            sumList.add(sum);
+            now = sb.toString();
+            result.add(now);
         }
 
-        List<Long> ascList = new ArrayList<>(sumList);
-        List<Long> descList = new ArrayList<>(sumList);
-        ascList.sort((a, b) -> Long.compare(a, b));
-        descList.sort((a, b) -> Long.compare(b, a));
-
-        long min = 0;
-        long max = 0;
-
-        int ascIdx = 0;
-        int descIdx = 0;
-        for (int i=0; i < k - 1; i++) {
-            min += ascList.get(ascIdx++);
-            max += descList.get(descIdx++);
-        }
-
-        return max - min;
+        return result;
     }
 }
