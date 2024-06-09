3e6fca7836e560b5469bb18136c1b64c4289e97c
EunChanNam <namhm23@gmail.com>
2024-06-09 13:33:18 +0900

    프로그래머스 도둑질

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -1,32 +1,38 @@
-import java.util.*;
 class Solution {
-    public int solution(int[][] triangles) {
+    public int solution(int[] money) {
         int answer = 0;
 
-        List<Integer[]> dp = new ArrayList<>();
-        for (int[] triangle : triangles) {
-            Integer[] arr = new Integer[triangle.length];
-            dp.add(arr);
-        }
-
-        dp.get(0)[0] = triangles[0][0];
-
-        for (int y=1; y < triangles.length; y++) {
-            Integer[] preDp = dp.get(y - 1);
-            Integer[] nowDp = dp.get(y);
-            for (int x=0; x < nowDp.length; x++) {
-                if (x == nowDp.length - 1) {
-                    nowDp[x] = triangles[y][x] + preDp[x - 1];
-                } else if (x == 0) {
-                    nowDp[x] = triangles[y][x] + preDp[x];
-                } else {
-                    int max = Math.max(preDp[x], preDp[x - 1]);
-                    nowDp[x] = triangles[y][x] + max;
-                }
-                answer = Math.max(answer, nowDp[x]);
-            }
-        }
+        answer = Math.max(getByDp(true, money), getByDp(false, money));
 
         return answer;
+    }
+
+    private int getByDp(boolean isFirstSelect, int[] money) {
+        int n = money.length;
+        int[] dp = new int[n];
+        int max = 0;
+        int i = 1;
+        if (isFirstSelect) {
+            dp[0] = money[0];
+            n--;
+            i++;
+        }
+
+        for (; i < n; i++) {
+            int preMax;
+            if (i - 2 < 0) {
+                preMax = 0;
+            } else if (i - 3 < 0) {
+                preMax = dp[i - 2];
+            } else {
+                preMax = Math.max(dp[i - 2], dp[i - 3]);
+            }
+
+            dp[i] = money[i] + preMax;
+
+            max = Math.max(dp[i], max);
+        }
+
+        return max;
     }
 }
