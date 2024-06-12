416d96687bc5491bd0b33e5477623032cd1f0dca
EunChanNam <namhm23@gmail.com>
2024-06-12 16:44:30 +0900

    프로그래머스 방의개수

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -1,38 +1,77 @@
+import java.util.*;
 class Solution {
-    public int solution(int[] money) {
+
+    private static class Point {
+        int y;
+        int x;
+        public Point(int y, int x) {
+            this.y = y;
+            this.x = x;
+        }
+    }
+
+    private int[][] dir = {{-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}};
+    public int solution(int[] arrows) {
         int answer = 0;
 
-        answer = Math.max(getByDp(true, money), getByDp(false, money));
+        Point now = new Point(0, 0);
+        Map<String, Integer> map = new HashMap<>();
+        map.put(getPoint(now), 0);
+
+        Set<String> set = new HashSet<>();
+
+        for (int arrow : arrows) {
+            int ny = now.y + dir[arrow][0];
+            int nx = now.x + dir[arrow][1];
+            int ny2 = now.y + dir[arrow][0] * 2;
+            int nx2 = now.x + dir[arrow][1] * 2;
+            Point next = new Point(ny, nx);
+            Point next2 = new Point(ny2, nx2);
+
+            String line1 = getLine(now, next);
+            String line2 = getLine(next, now);
+            String line3 = getLine(next, next2);
+            String line4 = getLine(next2, next);
+
+            if (!set.contains(line1) && !set.contains(line2)) {
+                String nextPoint = getPoint(next);
+                map.put(nextPoint, map.getOrDefault(nextPoint, -1) + 1);
+            }
+            if (!set.contains(line3) && !set.contains(line4)) {
+                String nextPoint = getPoint(next2);
+                map.put(nextPoint, map.getOrDefault(nextPoint, -1) + 1);
+            }
+
+            set.add(line1);
+            set.add(line2);
+            set.add(line3);
+            set.add(line4);
+
+            now.y = ny2;
+            now.x = nx2;
+        }
+
+        for (int cnt : map.values()) {
+            answer += cnt;
+        }
 
         return answer;
     }
 
-    private int getByDp(boolean isFirstSelect, int[] money) {
-        int n = money.length;
-        int[] dp = new int[n];
-        int max = 0;
-        int i = 1;
-        if (isFirstSelect) {
-            dp[0] = money[0];
-            n--;
-            i++;
-        }
+    private String getPoint(Point point) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(point.y).append("/");
+        sb.append(point.x);
+        return sb.toString();
+    }
 
-        for (; i < n; i++) {
-            int preMax;
-            if (i - 2 < 0) {
-                preMax = 0;
-            } else if (i - 3 < 0) {
-                preMax = dp[i - 2];
-            } else {
-                preMax = Math.max(dp[i - 2], dp[i - 3]);
-            }
+    private String getLine(Point a, Point b) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(a.y).append('/');
+        sb.append(a.x).append('/');
+        sb.append(b.y).append('/');
+        sb.append(b.x);
 
-            dp[i] = money[i] + preMax;
-
-            max = Math.max(dp[i], max);
-        }
-
-        return max;
+        return sb.toString();
     }
 }
