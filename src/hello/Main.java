4a1bbe6017f47c0554b6c0dfb725912218771bce
EunChanNam <namhm23@gmail.com>
2024-06-18 14:34:02 +0900

    LeetCode - Longest Palindromic Substring

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -1,42 +1,31 @@
-import java.util.*;
 class Solution {
 
-    private List<List<Integer>> list = new ArrayList<>();
+    public String longestPalindrome(String s) {
 
-    public boolean validPath(int n, int[][] edges, int source, int destination) {
-        for (int i=0; i < n; i++) {
-            list.add(new ArrayList<>());
+        for (int i=0; i < s.length(); i++) {
+            getMaxLengthStr(s, i, i + 1);
+            getMaxLengthStr(s, i, i + 2);
         }
 
-        for (int[] edge : edges) {
-            int a = edge[0];
-            int b = edge[1];
-            list.get(a).add(b);
-            list.get(b).add(a);
-        }
-
-        return findByBfs(source, destination, n);
+        return s.substring(maxLeft, maxRight + 1);
     }
 
-    private boolean findByBfs(int start, int goal, int n) {
-        boolean[] visit = new boolean[n];
-        visit[start] = true;
+    private int maxLeft = 0;
+    private int maxRight = 0;
+    private int maxLength = 0;
 
-        Queue<Integer> que = new ArrayDeque<>();
-        que.offer(start);
+    private void getMaxLengthStr(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
 
-        while (!que.isEmpty()) {
-            int now = que.poll();
-            if (now == goal) return true;
-
-            List<Integer> nowList = list.get(now);
-            for (int next : nowList) {
-                if (visit[next]) continue;
-                visit[next] = true;
-                que.offer(next);
+            int length = right - left;
+            if (length > maxLength) {
+                maxLeft = left;
+                maxRight = right;
+                maxLength = length;
             }
-        }
 
-        return false;
+            left--;
+            right++;
+        }
     }
 }
