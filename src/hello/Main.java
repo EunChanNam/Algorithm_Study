185e78baa94eac872c14055f82e2d71e287cd09b
EunChanNam <namhm23@gmail.com>
2024-06-20 14:00:02 +0900

    LeetCode - Minimum Lines to Represent a Line Chart

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -1,27 +1,36 @@
 import java.util.*;
 class Solution {
-    public String removeKdigits(String num, int k) {
-        if (num.length() == k) return "0";
+    public int minimumLines(int[][] params) {
+        List<List<Integer>> stockPrices = new ArrayList<>();
+        for (int[] param : params) {
+            List<Integer> temp = new ArrayList<>();
+            temp.add(param[0]);
+            temp.add(param[1]);
 
-        Stack<Character> stack = new Stack<>();
-        for (char ch : num.toCharArray()) {
-            while (!stack.isEmpty() && k > 0 && stack.peek() > ch) {
-                stack.pop();
-                k--;
+            stockPrices.add(temp);
+        }
+
+        if (stockPrices.size() == 1) return 0;
+
+        stockPrices.sort((a, b) -> {
+            if (a.get(0) == b.get(0)) {
+                return Integer.compare(a.get(1), b.get(1));
             }
-            stack.push(ch);
+            return Integer.compare(a.get(0), b.get(0));
+        });
+
+        int cnt = 1;
+        for (int i=1; i < stockPrices.size() - 1; i++) {
+            List<Integer> pre = stockPrices.get(i - 1);
+            List<Integer> now = stockPrices.get(i);
+            List<Integer> next = stockPrices.get(i + 1);
+
+            int gap1 = (now.get(0) - pre.get(0)) * (next.get(1) - now.get(1));
+            int gap2 = (now.get(1) - pre.get(1)) * (next.get(0) - now.get(0));
+
+            if (gap1 != gap2) cnt++;
         }
 
-        StringBuilder sb = new StringBuilder();
-        for (char ch : stack) {
-            sb.append(ch);
-        }
-        sb.setLength(sb.length() - k);
-
-        while (!sb.isEmpty() && sb.charAt(0) == '0') {
-            sb.deleteCharAt(0);
-        }
-
-        return sb.toString().isEmpty() ? "0" : sb.toString();
+        return cnt;
     }
 }
