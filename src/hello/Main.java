a7757dee92d3b7e47037d6a94bd7c074aac48da1
EunChanNam <namhm23@gmail.com>
2024-06-21 14:41:09 +0900

    LeetCode - Append K Integers With Minimal Sum

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -1,36 +1,31 @@
 import java.util.*;
 class Solution {
-    public int minimumLines(int[][] params) {
-        List<List<Integer>> stockPrices = new ArrayList<>();
-        for (int[] param : params) {
-            List<Integer> temp = new ArrayList<>();
-            temp.add(param[0]);
-            temp.add(param[1]);
+    public long minimalKSum(int[] nums, int k) {
 
-            stockPrices.add(temp);
-        }
+        Arrays.sort(nums);
 
-        if (stockPrices.size() == 1) return 0;
+        int now = 1;
+        long sum = 0L;
 
-        stockPrices.sort((a, b) -> {
-            if (a.get(0) == b.get(0)) {
-                return Integer.compare(a.get(1), b.get(1));
+        for (int num : nums) {
+            boolean flag = false;
+            while (num > now) {
+                if (k <= 0){
+                    flag = true;
+                    break;
+                }
+                sum += now++;
+                k--;
             }
-            return Integer.compare(a.get(0), b.get(0));
-        });
-
-        int cnt = 1;
-        for (int i=1; i < stockPrices.size() - 1; i++) {
-            List<Integer> pre = stockPrices.get(i - 1);
-            List<Integer> now = stockPrices.get(i);
-            List<Integer> next = stockPrices.get(i + 1);
-
-            int gap1 = (now.get(0) - pre.get(0)) * (next.get(1) - now.get(1));
-            int gap2 = (now.get(1) - pre.get(1)) * (next.get(0) - now.get(0));
-
-            if (gap1 != gap2) cnt++;
+            if (flag) break;
+            now = num + 1;
         }
 
-        return cnt;
+        while (k > 0) {
+            sum += now++;
+            k--;
+        }
+
+        return sum;
     }
 }
