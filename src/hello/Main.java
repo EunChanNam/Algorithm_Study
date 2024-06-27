64b35132650d85b1efbc69e6e69a5ca4b9634ef9
EunChanNam <namhm23@gmail.com>
2024-06-27 13:09:23 +0900

    LeetCode - Minimum Cost of a Path With Special Roads

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -1,70 +1,60 @@
-/**
- * // This is the interface that allows for creating nested lists.
- * // You should not implement it, or speculate about its implementation
- * public interface NestedInteger {
- *     // Constructor initializes an empty nested list.
- *     public NestedInteger();
- *
- *     // Constructor initializes a single integer.
- *     public NestedInteger(int value);
- *
- *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
- *     public boolean isInteger();
- *
- *     // @return the single integer that this NestedInteger holds, if it holds a single integer
- *     // Return null if this NestedInteger holds a nested list
- *     public Integer getInteger();
- *
- *     // Set this NestedInteger to hold a single integer.
- *     public void setInteger(int value);
- *
- *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
- *     public void add(NestedInteger ni);
- *
- *     // @return the nested list that this NestedInteger holds, if it holds a nested list
- *     // Return empty list if this NestedInteger holds a single integer
- *     public List<NestedInteger> getList();
- * }
- */
 import java.util.*;
 class Solution {
-    public NestedInteger deserialize(String s) {
-        // [ 를 만나면 스택에 List 추가
-        // 숫자를 만나면 스택에서 peek() 하고 거기 리스트에 숫자 추가
-        // ] 를 만나면 스택에 List가 있으면 거기에 add, 없으면 종료
 
-        Stack<NestedInteger> stack = new Stack<>();
-        for (int i=0; i < s.length(); i++) {
-            char ch = s.charAt(i);
+    private static class Node {
+        int y;
+        int x;
+        int cost;
 
-            if (ch == '[') {
-                stack.push(new NestedInteger());
+        public boolean equals(Object object) {
+            Node target = (Node) object;
+            return (this.y == target.y && this.x == target.x);
+        }
+
+        public int hashCode() {
+            return Objects.hash(y, x);
+        }
+
+        public Node(int y, int x, int cost) {
+            this.y = y;
+            this.x = x;
+            this.cost = cost;
+        }
+    }
+
+    public int minimumCost(int[] startArr, int[] targetArr, int[][] specialRoads) {
+
+        PriorityQueue<Node> que = new PriorityQueue<>((a, b) -> Integer.compare(a.cost, b.cost));
+        Node start = new Node(startArr[1], startArr[0], 0);
+        Node target = new Node(targetArr[1], targetArr[0], 0);
+        que.offer(start);
+
+        Set<Node> visit = new HashSet<>();
+
+        while (!que.isEmpty()) {
+            Node now = que.poll();
+            if (now.equals(target)) {
+                return now.cost;
             }
-            if (ch >= '0' && ch <= '9' || ch == '-') {
-                StringBuilder sb = new StringBuilder();
-                sb.append(ch);
 
-                i++;
-                while (i < s.length() && (s.charAt(i) >= '0' && s.charAt(i) <= '9')) {
-                    sb.append(s.charAt(i++));
-                }
-                i--;
+            if (visit.contains(now)) continue;
+            visit.add(now);
 
-                if (!stack.isEmpty()) {
-                    stack.peek().add(new NestedInteger(Integer.parseInt(sb.toString())));
-                } else {
-                    stack.push(new NestedInteger(Integer.parseInt(sb.toString())));
-                }
-            }
-            if (ch == ']') {
-                NestedInteger top = stack.pop();
-                if (top.isInteger()) top = new NestedInteger(top.getInteger());
-                if (!stack.isEmpty()) {
-                    stack.peek().add(top);
-                } else stack.push(top);
+            que.offer(new Node(target.y, target.x, now.cost + getDis(now, target)));
+            for (int[] road : specialRoads) {
+                int nextCost = now.cost + getDis(now, road[0], road[1]) + road[4];
+                que.offer(new Node(road[3], road[2], nextCost));
             }
         }
 
-        return stack.pop();
+        return -1;
+    }
+
+    public int getDis(Node a, Node b) {
+        return Math.abs(b.x - a.x) + Math.abs(b.y - a.y);
+    }
+
+    public int getDis(Node a, int targetX, int targetY) {
+        return Math.abs(targetX - a.x) + Math.abs(targetY - a.y);
     }
 }
