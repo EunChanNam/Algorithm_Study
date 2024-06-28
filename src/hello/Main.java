d0cf576e0badb5ba47b0fa050f586b62e89a0461
EunChanNam <namhm23@gmail.com>
2024-06-28 13:06:10 +0900

    LeetCode - Monotone Increasing Digits

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -1,60 +1,44 @@
 import java.util.*;
 class Solution {
+    public int monotoneIncreasingDigits(int n) {
+        String s = String.valueOf(n);
+        Stack<Character> stack = new Stack<>();
 
-    private static class Node {
-        int y;
-        int x;
-        int cost;
-
-        public boolean equals(Object object) {
-            Node target = (Node) object;
-            return (this.y == target.y && this.x == target.x);
-        }
-
-        public int hashCode() {
-            return Objects.hash(y, x);
-        }
-
-        public Node(int y, int x, int cost) {
-            this.y = y;
-            this.x = x;
-            this.cost = cost;
-        }
-    }
-
-    public int minimumCost(int[] startArr, int[] targetArr, int[][] specialRoads) {
-
-        PriorityQueue<Node> que = new PriorityQueue<>((a, b) -> Integer.compare(a.cost, b.cost));
-        Node start = new Node(startArr[1], startArr[0], 0);
-        Node target = new Node(targetArr[1], targetArr[0], 0);
-        que.offer(start);
-
-        Set<Node> visit = new HashSet<>();
-
-        while (!que.isEmpty()) {
-            Node now = que.poll();
-            if (now.equals(target)) {
-                return now.cost;
+        boolean isDecre = false;
+        for (char ch : s.toCharArray()) {
+            if (stack.isEmpty()) {
+                stack.push(ch);
+                continue;
             }
 
-            if (visit.contains(now)) continue;
-            visit.add(now);
+            if (isDecre) {
+                stack.push('9');
+                continue;
+            }
 
-            que.offer(new Node(target.y, target.x, now.cost + getDis(now, target)));
-            for (int[] road : specialRoads) {
-                int nextCost = now.cost + getDis(now, road[0], road[1]) + road[4];
-                que.offer(new Node(road[3], road[2], nextCost));
+            int decreaseCnt = 0;
+            Stack<Character> temp = new Stack<>();
+            while (!stack.isEmpty() && stack.peek() > ch) {
+                char top = stack.pop();
+                ch = --top;
+                temp.push(ch);
+                decreaseCnt++;
+                isDecre = true;
+            }
+            if (!temp.isEmpty()) {
+                stack.push(temp.pop());
+            }
+            for (int i=0; i < decreaseCnt; i++) {
+                stack.push('9');
+            }
+
+            if (decreaseCnt == 0) {
+                stack.push(ch);
             }
         }
 
-        return -1;
-    }
-
-    public int getDis(Node a, Node b) {
-        return Math.abs(b.x - a.x) + Math.abs(b.y - a.y);
-    }
-
-    public int getDis(Node a, int targetX, int targetY) {
-        return Math.abs(targetX - a.x) + Math.abs(targetY - a.y);
+        StringBuilder sb = new StringBuilder();
+        stack.forEach(ch -> sb.append(ch));
+        return Integer.parseInt(sb.toString());
     }
 }
