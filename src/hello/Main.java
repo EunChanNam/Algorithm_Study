37a9ce5a97fcb47064a3c9fe53b0428662681264
EunChanNam <namhm23@gmail.com>
2024-07-18 20:44:08 +0900

    백준 1446 지름길

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -5,18 +5,27 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    /**
-     * 수열에서 연속한 1개 이상의 수를 뽑았을 때 같은 수가 여러 번 등장하지 않는 경우의 수
-     * 중복없는 수열들을 구하면됨
-     * 구한 중복없는 수열들의 길이를 전부 합하면 정답
-     * 포인트는 현재까지 구한 수열에서 수를 빼는건 중복이 생길수없는걸 이용
-     */
+    private static class Road {
+
+        int start;
+        int end;
+        int cost;
+
+        public Road(int start, int end, int cost) {
+            this.start = start;
+            this.end = end;
+            this.cost = cost;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -24,27 +33,39 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n];
-        st = new StringTokenizer(br.readLine());
+        int d = Integer.parseInt(st.nextToken());
+
+        Map<Integer, List<Road>> map = new HashMap<>();
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
 
-        int left = 0;
-        int right = 0;
-        Set<Integer> set = new HashSet<>();
-        long answer = 0;
-
-        for (; left < n; left++) {
-            while (right < n) {
-                if (set.contains(arr[right])) break;
-                set.add(arr[right++]);
+            if (!map.containsKey(start)) {
+                map.put(start, new ArrayList<>());
             }
-            answer += right - left;
-            set.remove(arr[left]);
+
+            map.get(start).add(new Road(start, end, cost));
         }
 
-        bw.write(String.valueOf(answer));
+        int[] dp = new int[10001];
+        for (int i = 1; i < 10001; i++) {
+            dp[i] = Integer.MAX_VALUE;
+        }
+        for (int i = 0; i <= d; i++) {
+            if (i != 0) {
+                dp[i] = Math.min(dp[i], dp[i - 1] + 1);
+            }
+            if (map.containsKey(i)) {
+                List<Road> roads = map.get(i);
+                for (Road road : roads) {
+                    dp[road.end] = Math.min(dp[road.end], dp[i] + road.cost);
+                }
+            }
+        }
+
+        bw.write(String.valueOf(dp[d]));
 
         br.close();
         bw.close();
