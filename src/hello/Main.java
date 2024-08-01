61b32f715dd639dc128f8d0251efc0b83fc8cba6
EunChanNam <namhm23@gmail.com>
2024-08-01 12:37:43 +0900

    프로그래머스 가장 큰 수

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -1,73 +1,29 @@
-package hello;
+import java.util.*;
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        int n = numbers.length;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.StringTokenizer;
+        List<String> strs = new ArrayList<>();
 
-public class Main {
-
-    private static class Road {
-
-        int start;
-        int end;
-        int cost;
-
-        public Road(int start, int end, int cost) {
-            this.start = start;
-            this.end = end;
-            this.cost = cost;
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int n = Integer.parseInt(st.nextToken());
-        int d = Integer.parseInt(st.nextToken());
-
-        Map<Integer, List<Road>> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-
-            if (!map.containsKey(start)) {
-                map.put(start, new ArrayList<>());
-            }
-
-            map.get(start).add(new Road(start, end, cost));
+        for (int i=0; i < n; i++) {
+            strs.add(String.valueOf(numbers[i]));
         }
 
-        int[] dp = new int[10001];
-        for (int i = 1; i < 10001; i++) {
-            dp[i] = Integer.MAX_VALUE;
-        }
-        for (int i = 0; i <= d; i++) {
-            if (i != 0) {
-                dp[i] = Math.min(dp[i], dp[i - 1] + 1);
-            }
-            if (map.containsKey(i)) {
-                List<Road> roads = map.get(i);
-                for (Road road : roads) {
-                    dp[road.end] = Math.min(dp[road.end], dp[i] + road.cost);
-                }
-            }
+        strs.sort((a, b) -> {
+            String order1 = a + b;
+            String order2 = b + a;
+            return order2.compareTo(order1);
+        });
+
+        StringBuilder sb = new StringBuilder();
+        strs.forEach(str -> sb.append(str));
+
+        answer = sb.toString();
+        if (!answer.isEmpty() && answer.charAt(0) == '0') {
+            return "0";
         }
 
-        bw.write(String.valueOf(dp[d]));
-
-        br.close();
-        bw.close();
+        return answer;
     }
 }
