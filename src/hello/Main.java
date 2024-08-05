a6fedabbe8e30354f87ee905b7779d8fabaf2c83
EunChanNam <namhm23@gmail.com>
2024-08-05 11:45:59 +0900

    프로그래머스 소수 찾기

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -1,30 +1,68 @@
 import java.util.*;
 class Solution {
-    public int[] solution(int[] numbers) {
+    public int solution(String numbers) {
+        int answer = 0;
+        n = numbers.length();
 
-        Stack<Integer> stack = new Stack<>();
-        int n = numbers.length;
-
-        Stack<Integer> result = new Stack<>();
-        for (int i = n - 1; i >= 0; i--) {
-            int now = numbers[i];
-            while (!stack.isEmpty() && stack.peek() <= now) {
-                stack.pop();
-            }
-            if (!stack.isEmpty() && stack.peek() > now) {
-                result.push(stack.peek());
-            } else {
-                result.push(-1);
-            }
-
-            stack.push(now);
+        for (char ch : numbers.toCharArray()) {
+            list.add((int) ch - '0');
         }
 
-        int[] answer = new int[n];
-        for (int i=0; i < n; i++) {
-            answer[i] = result.pop();
-        }
+        visit = new boolean[n];
+        dfs(0);
+        answer = cnt;
 
         return answer;
+    }
+
+    private static int n;
+    private static Set<Integer> set = new HashSet<>();
+    private static List<Integer> list = new ArrayList<>();
+    private static int cnt = 0;
+    private static boolean[] visit;
+    private static StringBuilder sb = new StringBuilder();
+
+    private void dfs(int level) {
+        if (level == n + 1) return;
+
+        String str = sb.toString();
+        if (!str.isEmpty()) {
+            int num = Integer.parseInt(str);
+
+            if (isPrime(num) && !set.contains(num)) {
+                set.add(num);
+                cnt++;
+            }
+        }
+
+        for (int i=0; i < n; i++) {
+            if (visit[i]) continue;
+            int next = list.get(i);
+
+            visit[i] = true;
+            sb.append(next);
+            dfs(level + 1);
+            sb.deleteCharAt(sb.length() - 1);
+            visit[i] = false;
+        }
+    }
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        if (number == 2) {
+            return true;
+        }
+        if (number % 2 == 0) {
+            return false;
+        }
+        for (int i = 3; i <= Math.sqrt(number); i += 2) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
