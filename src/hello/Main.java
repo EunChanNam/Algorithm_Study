10926341a0994db63bae8aa44f4a6b408ed295c0
EunChanNam <namhm23@gmail.com>
2024-08-12 14:32:29 +0900

    LeetCode - Maximal Rectangle(히스토그랩 직사각형 넓이)

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -1,29 +1,49 @@
+import java.util.*;
 class Solution {
-    public int solution(String name) {
-        int answer = 0;
-
-        int move = name.length() - 1;
-        for (int i=0; i < name.length(); i++) {
-            char ch = name.charAt(i);
-            answer += getCost(ch);
-
-            int idx = i + 1;
-            int cnt = 0;
-            while (idx < name.length() && name.charAt(idx) == 'A') {
-                cnt++;
-                idx++;
-            }
-
-            move = Math.min(move, i - cnt + name.length() - 1);
-            move = Math.min(move, (name.length() - idx) - cnt + name.length() - 1);
+    private static class Node {
+        int height;
+        int idx;
+        public Node(int height, int idx) {
+            this.height = height;
+            this.idx = idx;
         }
-
-        return answer + move;
     }
 
-    private int getCost(char target) {
-        int a = target - 'A';
-        int b = 'Z' - target + 1;
-        return Math.min(a, b);
+    public int maximalRectangle(char[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int max = 0;
+        int[] height = new int[m];
+        for (int y=0; y < n; y++) {
+            for (int x=0; x < m; x++) {
+                char k = matrix[y][x];
+                if (k == '1') height[x]++;
+                else height[x] = 0;
+            }
+
+            List<Node> list = new ArrayList<>();
+            for (int x=0; x < m; x++) {
+                max = Math.max(max, height[x]);
+                if (height[x] == 0) list = new ArrayList<>();
+
+                boolean flag = false;
+                boolean flag2 = true;
+                int maxHeight = 0;
+                for (Node node : list) {
+                    maxHeight = Math.max(maxHeight, node.height);
+                    int witdh = x - node.idx + 1;
+                    int h = Math.min(node.height, height[x]);
+                    if (height[x] < node.height) {
+                        node.height = height[x];
+                    }
+
+                    max = Math.max(max, witdh * h);
+                }
+                if (list.isEmpty() || height[x] > maxHeight) list.add(new Node(height[x], x));
+            }
+        }
+
+        return max;
     }
 }
