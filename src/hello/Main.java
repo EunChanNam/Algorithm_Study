d586a74fc0e13b41003552f5785d0d97363fe003
EunChanNam <namhm23@gmail.com>
2024-08-28 12:50:21 +0900

    프로그래머스 혼자 놀기의 달인(v2)

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -1,189 +1,47 @@
-package hello;
+import java.util.*;
+import java.util.stream.Collectors;
+class Solution {
+    public int solution(int[] cards) {
+        int answer = 0;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
-
-public class Main {
-
-    private static int n;
-    private static int[][] map;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        map = new int[n][n];
-
-        for (int y = 0; y < n; y++) {
-            st = new StringTokenizer(br.readLine());
-            for (int x = 0; x < n; x++) {
-                map[y][x] = Integer.parseInt(st.nextToken());
-            }
+        int n = cards.length;
+        int[] uf = new int[n + 1];
+        for (int i=0; i <= n; i++) {
+            uf[i] = i;
         }
 
-        dfs(map, 0, 0);
+        for (int i=0; i < n; i++) {
+            int k = i + 1;
+            union(k, cards[i], uf);
+        }
 
-        bw.write(String.valueOf(max));
+        Map<Integer, Integer> map = new HashMap<>();
 
-        br.close();
-        bw.close();
+        for (int card : cards) {
+            int group = find(card, uf);
+            map.put(group, map.getOrDefault(group, 0) + 1);
+        }
+
+        List<Integer> list = map.values().stream().collect(Collectors.toList());
+        if (list.size() == 1) {
+            return answer;
+        }
+
+        list.sort((a, b) -> Integer.compare(b, a));
+        answer = list.get(0) * list.get(1);
+
+        return answer;
     }
 
-    private static int max = Integer.MIN_VALUE;
-    private static void dfs(int[][] map, int dir, int level) {
-        int[][] tempMap = copyMap(map);
-        if (level != 0) {
-            move(dir, tempMap);
-        }
-
-        if (level == 5) {
-            for (int y = 0; y < n; y++) {
-                for (int x = 0; x < n; x++) {
-                    max = Math.max(max, tempMap[y][x]);
-                }
-            }
-            return;
-        }
-
-        for (int i = 1; i <= 4; i++) {
-            dfs(tempMap, i, level + 1);
-        }
+    private int find(int a, int[] uf) {
+        if (uf[a] == a) return a;
+        return uf[a] = find(uf[a], uf);
     }
 
-    private static int[][] copyMap(int[][] origin) {
-        int[][] temp = new int[n][n];
-        for (int y = 0; y < n; y++) {
-            System.arraycopy(origin[y], 0, temp[y], 0, n);
-        }
-        return temp;
-    }
+    private void union(int a, int b, int[] uf) {
+        int fa = find(a, uf);
+        int fb = find(b, uf);
 
-    private static void move(int dir, int[][] map) {
-        if (dir == 1) {
-            for (int x = 0; x < n; x++) {
-                for (int i = 0; i < n; i++) {
-                    if (isZero(map[i][x])) {
-                        continue;
-                    }
-                    for (int j = i + 1; j < n; j++) {
-                        if (xMergeIf(map, x, i, j)) {
-                            continue;
-                        }
-                        break;
-                    }
-                }
-            }
-            for (int x = 0; x < n; x++) {
-                for (int i = 0; i < n; i++) {
-                    for (int j = i + 1; j < n; j++) {
-                        swapIf(i, x, j, x, map);
-                    }
-                }
-            }
-        } else if (dir == 2) {
-            for (int x = 0; x < n; x++) {
-                for (int i = n - 1; i >= 0; i--) {
-                    if (isZero(map[i][x])) {
-                        continue;
-                    }
-                    for (int j = i - 1; j >= 0; j--) {
-                        if (xMergeIf(map, x, i, j)) {
-                            continue;
-                        }
-                        break;
-                    }
-                }
-            }
-            for (int x = 0; x < n; x++) {
-                for (int i = n - 1; i >= 0; i--) {
-                    for (int j = i - 1; j >= 0; j--) {
-                        swapIf(i, x, j, x, map);
-                    }
-                }
-            }
-        } else if (dir == 3) {
-            for (int y = 0; y < n; y++) {
-                for (int i = 0; i < n; i++) {
-                    if (isZero(map[y][i])) {
-                        continue;
-                    }
-                    for (int j = i + 1; j < n; j++) {
-                        if (yMergeIf(map, y, i, j)) {
-                            continue;
-                        }
-                        break;
-                    }
-                }
-            }
-            for (int y = 0; y < n; y++) {
-                for (int i = 0; i < n; i++) {
-                    for (int j = i + 1; j < n; j++) {
-                        swapIf(y, i, y, j, map);
-                    }
-                }
-            }
-        } else if (dir == 4) {
-            for (int y = 0; y < n; y++) {
-                for (int i = n - 1; i >= 0; i--) {
-                    if (isZero(map[y][i])) {
-                        continue;
-                    }
-                    for (int j = i - 1; j >= 0; j--) {
-                        if (yMergeIf(map, y, i, j)) {
-                            continue;
-                        }
-                        break;
-                    }
-                }
-            }
-            for (int y = 0; y < n; y++) {
-                for (int i = n - 1; i >= 0; i--) {
-                    for (int j = i - 1; j >= 0; j--) {
-                        swapIf(y, i, y, j, map);
-                    }
-                }
-            }
-        }
-    }
-
-    private static boolean isZero(int value) {
-        return value == 0;
-    }
-
-    private static boolean yMergeIf(int[][] map, int y, int i, int j) {
-        int now = map[y][i];
-        int next = map[y][j];
-        if (next == 0) return true;
-        if (now == next) {
-            map[y][i] += map[y][j];
-            map[y][j] = 0;
-        }
-        return false;
-    }
-
-    private static boolean xMergeIf(int[][] map, int x, int i, int j) {
-        int now = map[i][x];
-        int next = map[j][x];
-        if (next == 0) return true;
-        if (now == next) {
-            map[i][x] += map[j][x];
-            map[j][x] = 0;
-        }
-        return false;
-    }
-
-    private static void swapIf(int y, int x, int ny, int nx, int[][] map) {
-        if (map[y][x] == 0 && map[ny][nx] != 0) {
-            int temp = map[y][x];
-            map[y][x] = map[ny][nx];
-            map[ny][nx] = temp;
-        }
+        if (fa != fb) uf[fa] = fb;
     }
 }
